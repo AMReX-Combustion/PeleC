a15c47eb7d606c450ea446db39ee3f5682ce69b7
Emmanuel Motheau <emotheau@lbl.gov>
2019-01-17 17:36:14 -0800

    WIP: removing pc_denfill to keep only pc_hypfill

---
+++ Source/PeleC_bcfill.cpp
@@ -0,0 +1,59 @@
+
+#include <AMReX_FArrayBox.H>
+#include <AMReX_Geometry.H>
+#include <AMReX_PhysBCFunct.H>
+#include "PeleC.H"
+#include "PeleC_F.H"
+
+using namespace amrex;
+
+//struct CnsFillExtDir
+//{
+//    AMREX_GPU_DEVICE
+//    void operator() (const IntVect& iv, FArrayBox& dest,
+//                     const int dcomp, const int numcomp,
+//                     GeometryData const& geom, const Real time,
+//                     const BCRec* bcr, const int bcomp,
+//                     const int orig_comp) const
+//        {
+//            // do something for external Dirichlet (BCType::ext_dir)
+//        }
+//};
+//
+//namespace {
+//    static CnsFillExtDir cns_fill_ext_dir;
+//    static GpuBndryFuncFab<CnsFillExtDir> gpu_bndry_func(cns_fill_ext_dir);
+//    static CpuBndryFuncFab cpu_bndry_func(nullptr); // Without EXT_DIR (e.g., inflow), we can pass a nullptr
+//}
+
+// bx                  : Cells outside physical domain and inside bx are filled.
+// data, dcomp, numcomp: Fill numcomp components of data starting from dcomp.
+// bcr, bcomp          : bcr[bcomp] specifies BC for component dcomp and so on.
+// scomp               : component index for dcomp as in the desciptor set up in CNS::variableSetUp.
+
+void pc_bcfill_hyp (Box const& bx, FArrayBox& data,
+                 const int dcomp, const int numcomp,
+                 Geometry const& geom, const Real time,
+                 const Vector<BCRec>& bcr, const int bcomp,
+                 const int scomp)
+{
+//AMREX_ALWAYS_ASSERT(Gpu::isManaged(&data));
+
+const int* domlo      = geom.Domain().loVect();
+const int* domhi      = geom.Domain().hiVect();
+const Real* dx  = geom.CellSize();
+
+const RealBox pbx  = RealBox(bx,geom.CellSize(),geom.ProbLo());
+const Real* xlo     = pbx.lo();
+
+const int* bc =  bcr[bcomp].data();
+
+std::cout << "DEBUG dcomp " << dcomp << "  numcomp " << numcomp << "  bcomp " << bcomp << std::endl;
+
+//amrex::Print() << data;
+
+pc_hypfill(BL_TO_FORTRAN_3D(data),ARLIM_3D(domlo),ARLIM_3D(domhi),ZFILL(dx),ZFILL(xlo),time,bc);
+
+
+
+}
--- Source/Src_nd/bc_fill_nd.F90
+++ Source/Src_nd/bc_fill_nd.F90
@@ -6,6 +6,47 @@ module bc_fill_module
 
 contains
 
+
+ subroutine pc_hypfill2(adv,adv_lo,adv_hi,domlo,domhi,delta,xlo,time,bc) &
+       bind(C, name="pc_hypfill2")
+
+    use meth_params_module, only: NVAR
+    use prob_params_module, only: dim
+
+    implicit none
+
+    include 'AMReX_bc_types.fi'
+
+    integer          :: adv_lo(3),adv_hi(3)
+    integer          :: bc(dim,2,*)
+    integer          :: domlo(3), domhi(3)
+    double precision :: delta(3), xlo(3)
+    double precision, value :: time
+    !double precision :: time
+    double precision :: adv(adv_lo(1):adv_hi(1),adv_lo(2):adv_hi(2),adv_lo(3):adv_hi(3),NVAR)
+
+    double precision :: x(3)
+    integer :: i, j, k, n
+    logical rho_only
+
+    !do n = 1,NVAR
+    !   call filcc_nd(adv(:,:,:,n),adv_lo,adv_hi,domlo,domhi,delta,xlo,bc(:,:,n))
+    !enddo
+
+write(*,*) ''
+write(*,*) 'HEY IT WORKS'
+write(*,*) 'HEY adv ',lbound(adv),ubound(adv)
+write(*,*) 'HEY adv lo ',adv_lo
+write(*,*) 'HEY adv hi ',adv_hi
+write(*,*) 'HEY delta ',delta
+write(*,*) 'HEY xlo ',xlo
+write(*,*) 'HEY time ',time
+write(*,*) 'HEY bc ',bc(1,1,1)
+
+  end subroutine pc_hypfill2
+
+
+
   ! All subroutines in this file must be threadsafe because they are called
   ! inside OpenMP parallel regions.
   
