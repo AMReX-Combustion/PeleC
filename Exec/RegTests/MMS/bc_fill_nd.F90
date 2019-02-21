AMReX-Combustion/PeleC

commit abbab1f47397deac34e56a01f8a3f5fa19cb0087

Emmanuel/gc nscbc cleaning (#20)
* First pass of cleaning, we don't need X separate routines for X dimensions

* Continue cleaning of GC_NSCBC

* Renaming working test cases

* Cleaning how periodicity and corners are checked

* bugfix PeleC_forcing

* Fixing problem with bcMask, now it is defining physical BC at cell faces and no longer cell center

* Cleaning set_bc_mask but problem in 3d

* prodata files were missing because of the gitignore

* WIP : Fix bcMask in 3D

* WIP : bug fix set_bc_mask

* bug fix ghost cells for bcMask

* Putting a 3D test case to test walls first, and then outflow and corners

* WIP : Change how the Riemann solvers in 3D see if we have a wall defined at faces

* Specific commit to keep my debugging stuff

* bug fix in set_bc_Mask

* Cleaning Riemann2d
WARNING: Riemann1D still need to be done

* WIP : Cleaning nscbc 2D routine with new generic functions

* WIP : Cleaning nscbc 2d

* WIP : Cleaning nscbc 2d

* Reorganization of the nscbc routine, it is much more readable now

* Changing the definition of waves for the Y direction in 2D, to make it consistent with corners

* Started to clean the corners in 2D. These generic functions are very useful and clear, I should have done that 2 years ago ...

* WIP : Cleaning corners

* The nscbc routine never looked so clean and so shiny !

* bugfix of a variable used before initialization

* fixing inflow/outflow corner in 2D

* Fixing the treatment of corners in 2D. Actually it turns out that we do not need the complicated coupling that is written in the AIAA paper and that providing transverse terms from one-sided derivative is enough. Hope it will work for any case

* Allowing wall/wall corners, but there is something wrong with walls in the y direction

* bug fix for walls in Y direction

* Putting back the full wall square test case

* Fixing NSCBC in 1D

* WIP: NSCBC 3D

* WIP : NSCBC 3D seems to work in X direction

Exec/RegTests/MMS/bc_fill_nd.F90
@@ -25,49 +25,34 @@ contains
 
     double precision :: x(3)
     integer :: i, j, k, n
-    logical rho_only
-
-    type (eos_t) :: eos_state
-
-    call build(eos_state)
 
     do n = 1,NVAR
        call filcc_nd(adv(:,:,:,n),adv_lo,adv_hi,domlo,domhi,delta,xlo,bc(:,:,n))
     enddo
 
-    ! The strategy here is to set Dirichlet condition for inflow and
-    ! outflow boundaries, and let the Riemann solver sort out the proper
-    ! upwinding.  However, this decision makes this routine look
-    ! somewhat non-orthodox, in that we need to set external values in
-    ! either case....how do we know it's Outflow?  We have to assume
-    ! that the setup routines converted Outflow to FOEXTRAP.
-
-    ! Set flag for bc function
-    rho_only = .FALSE.
-
     !     XLO
-    if ( (bc(1,1,1).eq.EXT_DIR.or.bc(1,1,1).eq.FOEXTRAP).and. adv_lo(1).lt.domlo(1)) then
+    if ( (bc(1,1,1).eq.EXT_DIR).and. adv_lo(1).lt.domlo(1)) then
        do i = adv_lo(1), domlo(1)-1
           x(1) = xlo(1) + delta(1)*(dble(i-adv_lo(1)) + 0.5d0)
           do j = adv_lo(2), adv_hi(2)
              x(2) = xlo(2) + delta(2)*(dble(j-adv_lo(2)) + 0.5d0)
              do k = adv_lo(3), adv_hi(3)
                 x(3) = xlo(3) + delta(3)*(dble(k-adv_lo(3)) + 0.5d0)
-                call bcnormal(x,adv(domlo(1),j,k,:),adv(i,j,k,:),1,+1,bc(1,1,1),eos_state,rho_only,time)
+                call bcnormal(x,adv(domlo(1),j,k,:),adv(i,j,k,:),1,+1,time)
              end do
           end do
        end do
     end if
 
     !     XHI
-    if ( (bc(1,2,1).eq.EXT_DIR.or.bc(1,2,1).eq.FOEXTRAP).and. adv_hi(1).gt.domhi(1)) then
+    if ( (bc(1,2,1).eq.EXT_DIR).and. adv_hi(1).gt.domhi(1)) then
        do i = domhi(1)+1, adv_hi(1)
           x(1) = xlo(1) + delta(1)*(dble(i-adv_lo(1)) + 0.5d0)
           do j = adv_lo(2), adv_hi(2)
              x(2) = xlo(2) + delta(2)*(dble(j-adv_lo(2)) + 0.5d0)
              do k = adv_lo(3), adv_hi(3)
                 x(3) = xlo(3) + delta(3)*(dble(k-adv_lo(3)) + 0.5d0)
-                call bcnormal(x,adv(domhi(1),j,k,:),adv(i,j,k,:),1,-1,bc(1,2,1),eos_state,rho_only,time)
+                call bcnormal(x,adv(domhi(1),j,k,:),adv(i,j,k,:),1,-1,time)
              end do
           end do
        end do
@@ -75,28 +60,28 @@ contains
 
     if (dim .gt. 1) then
        !     YLO
-       if ( (bc(2,1,1).eq.EXT_DIR.or.bc(2,1,1).eq.FOEXTRAP).and. adv_lo(2).lt.domlo(2)) then
+       if ( (bc(2,1,1).eq.EXT_DIR).and. adv_lo(2).lt.domlo(2)) then
           do i = adv_lo(1), adv_hi(1)
              x(1) = xlo(1) + delta(1)*(dble(i-adv_lo(1)) + 0.5d0)
              do j = adv_lo(2), domlo(2)-1
                 x(2) = xlo(2) + delta(2)*(dble(j-adv_lo(2)) + 0.5d0)
                 do k = adv_lo(3), adv_hi(3)
                    x(3) = xlo(3) + delta(3)*(dble(k-adv_lo(3)) + 0.5d0)
-                   call bcnormal(x,adv(i,domlo(2),k,:),adv(i,j,k,:),2,+1,bc(2,1,1),eos_state,rho_only,time)
+                   call bcnormal(x,adv(i,domlo(2),k,:),adv(i,j,k,:),2,+1,time)
                 end do
              end do
           end do
        end if
 
        !     YHI
-       if ( (bc(2,2,1).eq.EXT_DIR.or.bc(2,2,1).eq.FOEXTRAP).and. adv_hi(2).gt.domhi(2)) then
+       if ( (bc(2,2,1).eq.EXT_DIR).and. adv_hi(2).gt.domhi(2)) then
           do i = adv_lo(1), adv_hi(1)
              x(1) = xlo(1) + delta(1)*(dble(i-adv_lo(1)) + 0.5d0)
              do j = domhi(2)+1, adv_hi(2)
                 x(2) = xlo(2) + delta(2)*(dble(j-adv_lo(2)) + 0.5d0)
                 do k = adv_lo(3), adv_hi(3)
                    x(3) = xlo(3) + delta(3)*(dble(k-adv_lo(3)) + 0.5d0)
-                   call bcnormal(x,adv(i,domhi(2),k,:),adv(i,j,k,:),2,-1,bc(2,2,1),eos_state,rho_only,time)
+                   call bcnormal(x,adv(i,domhi(2),k,:),adv(i,j,k,:),2,-1,time)
                 end do
              end do
           end do
@@ -104,166 +89,38 @@ contains
 
        if (dim .gt. 2) then
           !     ZLO
-          if ( (bc(3,1,1).eq.EXT_DIR.or.bc(3,1,1).eq.FOEXTRAP).and. adv_lo(3).lt.domlo(3)) then
+          if ( (bc(3,1,1).eq.EXT_DIR).and. adv_lo(3).lt.domlo(3)) then
              do i = adv_lo(1), adv_hi(1)
                 x(1) = xlo(1) + delta(1)*(dble(i-adv_lo(1)) + 0.5d0)
                 do j = adv_lo(2), adv_hi(2)
                    x(2) = xlo(2) + delta(2)*(dble(j-adv_lo(2)) + 0.5d0)
                    do k = adv_lo(3), domlo(3)-1
                       x(3) = xlo(3) + delta(3)*(dble(k-adv_lo(3)) + 0.5d0)
-                      call bcnormal(x,adv(i,j,domlo(3),:),adv(i,j,k,:),3,+1,bc(3,1,1),eos_state,rho_only,time)
+                      call bcnormal(x,adv(i,j,domlo(3),:),adv(i,j,k,:),3,+1,time)
                    end do
                 end do
              end do
           end if
 
           !     ZHI
-          if ( (bc(3,2,1).eq.EXT_DIR.or.bc(3,2,1).eq.FOEXTRAP).and. adv_hi(3).gt.domhi(3)) then
+          if ( (bc(3,2,1).eq.EXT_DIR).and. adv_hi(3).gt.domhi(3)) then
              do i = adv_lo(1), adv_hi(1)
                 x(1) = xlo(1) + delta(1)*(dble(i-adv_lo(1)) + 0.5d0)
                 do j = adv_lo(2), adv_hi(2)
                    x(2) = xlo(2) + delta(2)*(dble(j-adv_lo(2)) + 0.5d0)
                    do k = domhi(3)+1, adv_hi(3)
                       x(3) = xlo(3) + delta(3)*(dble(k-adv_lo(3)) + 0.5d0)
-                      call bcnormal(x,adv(i,j,domhi(3),:),adv(i,j,k,:),3,-1,bc(3,2,1),eos_state,rho_only,time)
+                      call bcnormal(x,adv(i,j,domhi(3),:),adv(i,j,k,:),3,-1,time)
                    end do
                 end do
              end do
           end if
        end if
     end if
-
-    call destroy(eos_state)
 
   end subroutine pc_hypfill
 
-  subroutine pc_denfill(adv,adv_lo,adv_hi,domlo,domhi,delta,xlo,time,bc) &
-       bind(C, name="pc_denfill")
-
-    use prob_params_module, only: dim
-    use eos_type_module
-
-    implicit none
-
-    include 'AMReX_bc_types.fi'
-
-    integer          :: adv_lo(3),adv_hi(3)
-    integer          :: bc(dim,2,*)
-    integer          :: domlo(3), domhi(3)
-    double precision :: delta(3), xlo(3), time
-    double precision :: adv(adv_lo(1):adv_hi(1),adv_lo(2):adv_hi(2),adv_lo(3):adv_hi(3))
-
-    double precision :: x(3)
-    logical rho_only
-    integer :: i,j,k
-
-    type (eos_t) :: eos_state
-
-    call build(eos_state)
-
-    call filcc_nd(adv,adv_lo,adv_hi,domlo,domhi,delta,xlo,bc)
-
-    ! Note: this function should not be needed, technically, but is
-    ! provided to filpatch because there are many times in the algorithm
-    ! when just the density is needed.  We try to rig up the filling so
-    ! that the same function is called here and in hypfill where all the
-    ! states are filled.
-
-    rho_only = .TRUE.
-
-    !     XLO
-    if ( (bc(1,1,1).eq.EXT_DIR.or.bc(1,1,1).eq.FOEXTRAP).and.adv_lo(1).lt.domlo(1)) then
-       do i = adv_lo(1), domlo(1)-1
-          x(1) = xlo(1) + delta(1)*(dble(i-adv_lo(1)) + 0.5d0)
-          do j = adv_lo(2), adv_hi(2)
-             x(2) = xlo(2) + delta(2)*(dble(j-adv_lo(2)) + 0.5d0)
-             do k = adv_lo(3), adv_hi(3)
-                x(3) = xlo(3) + delta(3)*(dble(k-adv_lo(3)) + 0.5d0)
-                call bcnormal(x,adv(domlo(1),j,k),adv(i,j,k),1,+1,bc(1,1,1),eos_state,rho_only,time)
-             end do
-          end do
-       end do
-    end if
-
-    !     XHI
-    if ( (bc(1,2,1).eq.EXT_DIR.or.bc(1,2,1).eq.FOEXTRAP).and.adv_hi(1).gt.domhi(1)) then
-       do i = domhi(1)+1, adv_hi(1)
-          x(1) = xlo(1) + delta(1)*(dble(i-adv_lo(1)) + 0.5d0)
-          do j = adv_lo(2), adv_hi(2)
-             x(2) = xlo(2) + delta(2)*(dble(j-adv_lo(2)) + 0.5d0)
-             do k = adv_lo(3), adv_hi(3)
-                x(3) = xlo(3) + delta(3)*(dble(k-adv_lo(3)) + 0.5d0)
-                call bcnormal(x,adv(domhi(1),j,k),adv(i,j,k),1,-1,bc(1,2,1),eos_state,rho_only,time)
-             end do
-          end do
-       end do
-    end if
-
-    if (dim > 1) then
-       !     YLO
-       if ( (bc(2,1,1).eq.EXT_DIR.or.bc(2,1,1).eq.FOEXTRAP).and.adv_lo(2).lt.domlo(2)) then
-          do i = adv_lo(1), adv_hi(1)
-             x(1) = xlo(1) + delta(1)*(dble(i-adv_lo(1)) + 0.5d0)
-             do j = adv_lo(2), domlo(2)-1
-                x(2) = xlo(2) + delta(2)*(dble(j-adv_lo(2)) + 0.5d0)
-                do k = adv_lo(3), adv_hi(3)
-                   x(3) = xlo(3) + delta(3)*(dble(k-adv_lo(3)) + 0.5d0)
-                   call bcnormal(x,adv(i,domlo(2),k),adv(i,j,k),2,+1,bc(2,1,1),eos_state,rho_only,time)
-                end do
-             end do
-          end do
-       end if
-
-       !     YHI
-       if ( (bc(2,2,1).eq.EXT_DIR.or.bc(2,2,1).eq.FOEXTRAP).and.adv_hi(2).gt.domhi(2)) then
-          do i = adv_lo(1), adv_hi(1)
-             x(1) = xlo(1) + delta(1)*(dble(i-adv_lo(1)) + 0.5d0)
-             do j = domhi(2)+1, adv_hi(2)
-                x(2) = xlo(2) + delta(2)*(dble(j-adv_lo(2)) + 0.5d0)
-                do k = adv_lo(3), adv_hi(3)
-                   x(3) = xlo(3) + delta(3)*(dble(k-adv_lo(3)) + 0.5d0)
-                   call bcnormal(x,adv(i,domhi(2),k),adv(i,j,k),2,-1,bc(2,2,1),eos_state,rho_only,time)
-                end do
-             end do
-          end do
-       end if
-
-       if (dim > 2) then
-          !     ZLO
-          if ( (bc(3,1,1).eq.EXT_DIR.or.bc(3,1,1).eq.FOEXTRAP).and.adv_lo(3).lt.domlo(3)) then
-             do i = adv_lo(1), adv_hi(1)
-                x(1) = xlo(1) + delta(1)*(dble(i-adv_lo(1)) + 0.5d0)
-                do j = adv_lo(2), adv_hi(2)
-                   x(2) = xlo(2) + delta(2)*(dble(j-adv_lo(2)) + 0.5d0)
-                   do k = adv_lo(3), domlo(3)-1
-                      x(3) = xlo(3) + delta(3)*(dble(k-adv_lo(3)) + 0.5d0)
-                      call bcnormal(x,adv(i,j,domlo(3)),adv(i,j,k),3,+1,bc(3,1,1),eos_state,rho_only,time)
-                   end do
-                end do
-             end do
-          end if
-
-          !     ZHI
-          if ( (bc(3,2,1).eq.EXT_DIR.or.bc(3,2,1).eq.FOEXTRAP).and.adv_hi(3).gt.domhi(3)) then
-             do i = adv_lo(1), adv_hi(1)
-                x(1) = xlo(1) + delta(1)*(dble(i-adv_lo(1)) + 0.5d0)
-                do j = adv_lo(2), adv_hi(2)
-                   x(2) = xlo(2) + delta(2)*(dble(j-adv_lo(2)) + 0.5d0)
-                   do k = domhi(3)+1, adv_hi(3)
-                      x(3) = xlo(3) + delta(3)*(dble(k-adv_lo(3)) + 0.5d0)
-                      call bcnormal(x,adv(i,j,domhi(3)),adv(i,j,k),3,-1,bc(3,2,1),eos_state,rho_only,time)
-                   end do
-                end do
-             end do
-          end if
-       end if
-    end if
-
-    call destroy(eos_state)
-
-  end subroutine pc_denfill
-
-  subroutine bcnormal(x,u_int,u_ext,dir,sgn,bc,eos_state,rho_only,time)
+  subroutine bcnormal(x,u_int,u_ext,dir,sgn,time,bc_type,bc_params,bc_target)
 
     use probdata_module
     use eos_type_module
@@ -271,6 +128,7 @@ contains
     use meth_params_module, only : URHO, UMX, UMY, UMZ, UTEMP, UEDEN, UEINT, UFS, NVAR
     use network, only: nspec, naux
     use prob_params_module, only : problo, probhi, dim
+    integer :: flag_nscbc
 
     use bl_constants_module, only: M_PI, HALF
 
@@ -285,24 +143,45 @@ contains
     double precision :: x(3),vint(3)
     double precision :: u_int(NVAR),u_ext(NVAR)
     double precision :: time
-    logical rho_only
     integer :: dir,sgn,bc
     type (eos_t) :: eos_state
     double precision :: rho,u,v,w,p,eint
 
+    integer, optional, intent(out) :: bc_type
+    double precision, optional, intent(out) :: bc_params(6)
+    double precision, optional, intent(out) :: bc_target(5)
+
+    flag_nscbc = 0
+
+    ! When optional arguments are present, GC-NSCBC is activated
+    ! Generic values are auto-filled for numerical parameters,
+    ! but should be set by the user for each BC
+    ! Note that in the impose_NSCBC_xD.f90 routine, not all parameters are used in same time
+    if (present(bc_type).and.present(bc_params).and.present(bc_target)) then
+
+      flag_nscbc = 1
+      relax_U = 0.5d0 ! For inflow only, relax parameter for x_velocity
+      relax_V = 0.5d0 ! For inflow only, relax parameter for y_velocity
+      relax_W = 0.5d0 ! For inflow only, relax parameter for z_velocity
+      relax_T = 0.2d0 ! For inflow only, relax parameter for temperature
+      beta = 0.2d0  ! Control the contribution of transverse terms
+      sigma_out = 0.25d0 ! For outflow only, relax parameter
+      which_bc_type = Interior ! This is to ensure that nothing will be done if the user don't set anything
+    endif
+    
+    if (flag_nscbc == 1) then
+      call bl_abort("For the MMS test case, NSCBC should be turned off")
+    endif
 #ifdef USE_MASA
 
     call build(eos_state)
 
-
+    if (dir == 1)
     ! inflow
-    if(bc .eq. EXT_DIR .and. sgn .eq. 1) then
+      if(sgn .eq. -1) then
 
-       rho = masa_eval_3d_exact_rho(x(1),x(2),x(3))
+          rho = masa_eval_3d_exact_rho(x(1),x(2),x(3))
 
-       if(rho_only .eqv. .true.) then
-          u_ext(URHO) = rho
-       else
           u = masa_eval_3d_exact_u(x(1),x(2),x(3))
           v = masa_eval_3d_exact_v(x(1),x(2),x(3))
           w = masa_eval_3d_exact_w(x(1),x(2),x(3))
@@ -323,16 +202,12 @@ contains
           u_ext(UTEMP)           = eos_state % T
           u_ext(UEINT)           = rho * eint
           u_ext(UEDEN)           = rho * (eint + HALF * (u**2 + v**2 + w**2))
-       endif
 
     ! outflow
-    else if(bc .eq. EXT_DIR .and. sgn .eq. -1) then
+      else if(sgn .eq. 1) then
 
-       rho = masa_eval_3d_exact_rho(x(1),x(2),x(3))
+          rho = masa_eval_3d_exact_rho(x(1),x(2),x(3))
 
-       if(rho_only .eqv. .true.) then
-          u_ext(URHO) = rho
-       else
           u = masa_eval_3d_exact_u(x(1),x(2),x(3))
           v = masa_eval_3d_exact_v(x(1),x(2),x(3))
           w = masa_eval_3d_exact_w(x(1),x(2),x(3))
@@ -353,13 +228,72 @@ contains
           u_ext(UTEMP)           = eos_state % T
           u_ext(UEINT)           = rho * eint
           u_ext(UEDEN)           = rho * (eint + HALF * (u**2 + v**2 + w**2))
-       endif
 
+      endif
+    endif
+    
+     if (dir == 3)
+    ! inflow
+      if(sgn .eq. 1) then
+
+          rho = masa_eval_3d_exact_rho(x(1),x(2),x(3))
+
+          u = masa_eval_3d_exact_u(x(1),x(2),x(3))
+          v = masa_eval_3d_exact_v(x(1),x(2),x(3))
+          w = masa_eval_3d_exact_w(x(1),x(2),x(3))
+          p = masa_eval_3d_exact_p(x(1),x(2),x(3))
+
+          eos_state % rho = rho
+          eos_state % p = p
+          eos_state % massfrac    = 0.d0
+          eos_state % massfrac(1) = 1.d0
+          call eos_rp(eos_state)
+          eint = eos_state % e
+
+          u_ext(URHO)            = rho
+          u_ext(UFS:UFS+nspec-1) = rho * eos_state % massfrac(1:nspec)
+          u_ext(UMX)             = rho * u
+          u_ext(UMY)             = rho * v
+          u_ext(UMZ)             = rho * w
+          u_ext(UTEMP)           = eos_state % T
+          u_ext(UEINT)           = rho * eint
+          u_ext(UEDEN)           = rho * (eint + HALF * (u**2 + v**2 + w**2))
+
+    ! outflow
+      else if(sgn .eq. -1) then
+
+          rho = masa_eval_3d_exact_rho(x(1),x(2),x(3))
+
+          u = masa_eval_3d_exact_u(x(1),x(2),x(3))
+          v = masa_eval_3d_exact_v(x(1),x(2),x(3))
+          w = masa_eval_3d_exact_w(x(1),x(2),x(3))
+          p = masa_eval_3d_exact_p(x(1),x(2),x(3))
+
+          eos_state % rho = rho
+          eos_state % p = p
+          eos_state % massfrac    = 0.d0
+          eos_state % massfrac(1) = 1.d0
+          call eos_rp(eos_state)
+          eint = eos_state % e
+
+          u_ext(URHO)            = rho
+          u_ext(UFS:UFS+nspec-1) = rho * eos_state % massfrac(1:nspec)
+          u_ext(UMX)             = rho * u
+          u_ext(UMY)             = rho * v
+          u_ext(UMZ)             = rho * w
+          u_ext(UTEMP)           = eos_state % T
+          u_ext(UEINT)           = rho * eint
+          u_ext(UEDEN)           = rho * (eint + HALF * (u**2 + v**2 + w**2))
+
+    endif
     endif
 
 #else
     call bl_error('MASA is not turned on. Turn on with USE_MASA=TRUE.')
 #endif
+
+    call destroy(eos_state)
+
   end subroutine bcnormal
 
   subroutine pc_reactfill(adv,adv_lo,adv_hi,domlo,domhi,delta,xlo,time,bc) &

commit 725ad8b295e8e0dd9d408a1678e3a942db8a9873

Fixes MMS compile errors introduced in #20.

Exec/RegTests/MMS/bc_fill_nd.F90
@@ -127,8 +127,8 @@ contains
     use eos_module
     use meth_params_module, only : URHO, UMX, UMY, UMZ, UTEMP, UEDEN, UEINT, UFS, NVAR
     use network, only: nspec, naux
-    use prob_params_module, only : problo, probhi, dim
-    integer :: flag_nscbc
+    use prob_params_module, only : Interior, Inflow, Outflow, SlipWall, NoSlipWall, &
+                                   problo, probhi, dim
 
     use bl_constants_module, only: M_PI, HALF
 
@@ -150,6 +150,9 @@ contains
     integer, optional, intent(out) :: bc_type
     double precision, optional, intent(out) :: bc_params(6)
     double precision, optional, intent(out) :: bc_target(5)
+
+    double precision :: relax_U, relax_V, relax_W, relax_T, beta, sigma_out
+    integer :: flag_nscbc, which_bc_type
 
     flag_nscbc = 0
 
@@ -176,117 +179,28 @@ contains
 
     call build(eos_state)
 
-    if (dir == 1)
-    ! inflow
-      if(sgn .eq. -1) then
-
-          rho = masa_eval_3d_exact_rho(x(1),x(2),x(3))
-
-          u = masa_eval_3d_exact_u(x(1),x(2),x(3))
-          v = masa_eval_3d_exact_v(x(1),x(2),x(3))
-          w = masa_eval_3d_exact_w(x(1),x(2),x(3))
-          p = masa_eval_3d_exact_p(x(1),x(2),x(3))
-
-          eos_state % rho = rho
-          eos_state % p = p
-          eos_state % massfrac    = 0.d0
-          eos_state % massfrac(1) = 1.d0
-          call eos_rp(eos_state)
-          eint = eos_state % e
-
-          u_ext(URHO)            = rho
-          u_ext(UFS:UFS+nspec-1) = rho * eos_state % massfrac(1:nspec)
-          u_ext(UMX)             = rho * u
-          u_ext(UMY)             = rho * v
-          u_ext(UMZ)             = rho * w
-          u_ext(UTEMP)           = eos_state % T
-          u_ext(UEINT)           = rho * eint
-          u_ext(UEDEN)           = rho * (eint + HALF * (u**2 + v**2 + w**2))
-
-    ! outflow
-      else if(sgn .eq. 1) then
-
-          rho = masa_eval_3d_exact_rho(x(1),x(2),x(3))
-
-          u = masa_eval_3d_exact_u(x(1),x(2),x(3))
-          v = masa_eval_3d_exact_v(x(1),x(2),x(3))
-          w = masa_eval_3d_exact_w(x(1),x(2),x(3))
-          p = masa_eval_3d_exact_p(x(1),x(2),x(3))
-
-          eos_state % rho = rho
-          eos_state % p = p
-          eos_state % massfrac    = 0.d0
-          eos_state % massfrac(1) = 1.d0
-          call eos_rp(eos_state)
-          eint = eos_state % e
-
-          u_ext(URHO)            = rho
-          u_ext(UFS:UFS+nspec-1) = rho * eos_state % massfrac(1:nspec)
-          u_ext(UMX)             = rho * u
-          u_ext(UMY)             = rho * v
-          u_ext(UMZ)             = rho * w
-          u_ext(UTEMP)           = eos_state % T
-          u_ext(UEINT)           = rho * eint
-          u_ext(UEDEN)           = rho * (eint + HALF * (u**2 + v**2 + w**2))
-
-      endif
-    endif
+    rho = masa_eval_3d_exact_rho(x(1),x(2),x(3))
     
-     if (dir == 3)
-    ! inflow
-      if(sgn .eq. 1) then
+    u = masa_eval_3d_exact_u(x(1),x(2),x(3))
+    v = masa_eval_3d_exact_v(x(1),x(2),x(3))
+    w = masa_eval_3d_exact_w(x(1),x(2),x(3))
+    p = masa_eval_3d_exact_p(x(1),x(2),x(3))
+    
+    eos_state % rho = rho
+    eos_state % p = p
+    eos_state % massfrac    = 0.d0
+    eos_state % massfrac(1) = 1.d0
+    call eos_rp(eos_state)
+    eint = eos_state % e
 
-          rho = masa_eval_3d_exact_rho(x(1),x(2),x(3))
-
-          u = masa_eval_3d_exact_u(x(1),x(2),x(3))
-          v = masa_eval_3d_exact_v(x(1),x(2),x(3))
-          w = masa_eval_3d_exact_w(x(1),x(2),x(3))
-          p = masa_eval_3d_exact_p(x(1),x(2),x(3))
-
-          eos_state % rho = rho
-          eos_state % p = p
-          eos_state % massfrac    = 0.d0
-          eos_state % massfrac(1) = 1.d0
-          call eos_rp(eos_state)
-          eint = eos_state % e
-
-          u_ext(URHO)            = rho
-          u_ext(UFS:UFS+nspec-1) = rho * eos_state % massfrac(1:nspec)
-          u_ext(UMX)             = rho * u
-          u_ext(UMY)             = rho * v
-          u_ext(UMZ)             = rho * w
-          u_ext(UTEMP)           = eos_state % T
-          u_ext(UEINT)           = rho * eint
-          u_ext(UEDEN)           = rho * (eint + HALF * (u**2 + v**2 + w**2))
-
-    ! outflow
-      else if(sgn .eq. -1) then
-
-          rho = masa_eval_3d_exact_rho(x(1),x(2),x(3))
-
-          u = masa_eval_3d_exact_u(x(1),x(2),x(3))
-          v = masa_eval_3d_exact_v(x(1),x(2),x(3))
-          w = masa_eval_3d_exact_w(x(1),x(2),x(3))
-          p = masa_eval_3d_exact_p(x(1),x(2),x(3))
-
-          eos_state % rho = rho
-          eos_state % p = p
-          eos_state % massfrac    = 0.d0
-          eos_state % massfrac(1) = 1.d0
-          call eos_rp(eos_state)
-          eint = eos_state % e
-
-          u_ext(URHO)            = rho
-          u_ext(UFS:UFS+nspec-1) = rho * eos_state % massfrac(1:nspec)
-          u_ext(UMX)             = rho * u
-          u_ext(UMY)             = rho * v
-          u_ext(UMZ)             = rho * w
-          u_ext(UTEMP)           = eos_state % T
-          u_ext(UEINT)           = rho * eint
-          u_ext(UEDEN)           = rho * (eint + HALF * (u**2 + v**2 + w**2))
-
-    endif
-    endif
+    u_ext(URHO)            = rho
+    u_ext(UFS:UFS+nspec-1) = rho * eos_state % massfrac(1:nspec)
+    u_ext(UMX)             = rho * u
+    u_ext(UMY)             = rho * v
+    u_ext(UMZ)             = rho * w
+    u_ext(UTEMP)           = eos_state % T
+    u_ext(UEINT)           = rho * eint
+    u_ext(UEDEN)           = rho * (eint + HALF * (u**2 + v**2 + w**2))
 
 #else
     call bl_error('MASA is not turned on. Turn on with USE_MASA=TRUE.')