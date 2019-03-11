8bae75412710b492e8adc0266add3d5f2ce309ce
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2019-03-11 13:48:44 -0600

    Better description of HIT and TG vortex cases.

--- Exec/RegTests/HIT/gen_hit_ic.py
+++ Exec/RegTests/HIT/gen_hit_ic.py
@@ -10,7 +10,6 @@
 #   3. inverse Fourier transform of the velocity fluctuations (512^3 grid)
 #   4. velocity fluctuations resampled on the desired grid (N^3)
 #
-#
 # The velocity fluctuations are normalized by urms0 so to get the
 # actual velocity fluctuations, one must multiply these velocities by
 # the appropriate urms0.
@@ -35,38 +34,39 @@ import scipy.interpolate as spi
 #
 # ========================================================================
 parser = argparse.ArgumentParser(
-    description='Generate the velocity fluctuations for the HIT IC')
+    description="Generate the velocity fluctuations for the HIT IC"
+)
 parser.add_argument(
-    '-k0', help='Wave number containing highest energy', type=float,
-    default=4.0)
-parser.add_argument('-N', help='Resolution', type=int, default=16)
+    "-k0", help="Wave number containing highest energy", type=float, default=4.0
+)
+parser.add_argument("-N", help="Resolution", type=int, default=16)
 parser.add_argument(
-    '-s', '--seed', help='Random number generator seed', type=int, default=42)
+    "-s", "--seed", help="Random number generator seed", type=int, default=42
+)
 parser.add_argument(
-    '-p', '--plot', help='Save a plot of the x-velocity', action='store_true')
+    "-p", "--plot", help="Save a plot of the x-velocity", action="store_true"
+)
 args = parser.parse_args()
+
 
 # ========================================================================
 #
 # Function definitions
 #
 # ========================================================================
-
-
 def div0(a, b):
     """ Ignore division by 0, just replace it by 0,
 
     From: http://stackoverflow.com/questions/26248654/numpy-return-0-with-divide-by-zero
     e.g. div0( [-1, 0, 1], 0 ) -> [0, 0, 0]
     """
-    with np.errstate(divide='ignore', invalid='ignore'):
+    with np.errstate(divide="ignore", invalid="ignore"):
         c = np.true_divide(a, b)
-        c[~ np.isfinite(c)] = 0  # -inf inf NaN
+        c[~np.isfinite(c)] = 0  # -inf inf NaN
     return c
 
+
 # ========================================================================
-
-
 def abs2(x):
     """This is equivalent to np.abs(x)**2 or x*np.conj(x)
 
@@ -74,7 +74,7 @@ def abs2(x):
     import numba
     @numba.vectorize([numba.float64(numba.complex128),numba.float32(numba.complex64)])
     """
-    return x.real**2 + x.imag**2
+    return x.real ** 2 + x.imag ** 2
 
 
 # ========================================================================
@@ -99,20 +99,20 @@ dx = L / N
 
 # Only work if N and args.N are even
 if not ((args.N % 2 == 0) and N % 2 == 0):
-    print('N or args.N is not even. Exiting')
+    print("N or args.N is not even. Exiting")
     sys.exit(1)
 
 # Get cell centered values and meshed grid
 x = np.linspace(xs, xe, N + 1)
 xc = (x[1:] + x[:-1]) / 2  # get cell center coordinates
-X, Y, Z = np.meshgrid(xc, xc, xc, indexing='ij')
+X, Y, Z = np.meshgrid(xc, xc, xc, indexing="ij")
 
 # Get the wave numbers and associated quantities
 k = np.concatenate((np.arange(halfN), np.arange(-halfN, 0, 1)), axis=0)
 khalf = np.arange(halfN + 1)
-k1, k2, k3 = np.meshgrid(k, k, khalf, indexing='ij')
-kmag = np.sqrt(k1**2 + k2**2 + k3**2)
-k12 = np.sqrt(k1**2 + k2**2)
+k1, k2, k3 = np.meshgrid(k, k, khalf, indexing="ij")
+kmag = np.sqrt(k1 ** 2 + k2 ** 2 + k3 ** 2)
+k12 = np.sqrt(k1 ** 2 + k2 ** 2)
 k1k12 = div0(k1, k12)
 k2k12 = div0(k2, k12)
 k3kmag = div0(k3, kmag)
@@ -127,8 +127,13 @@ k12kmag = div0(k12, kmag)
 # wf = np.copy(uf)
 
 # Energy spectrum
-Ek = 16.0 * np.sqrt(2.0 / np.pi) * (kmag**4) / (args.k0**5) * \
-    np.exp(-2.0 * (kmag**2) / (args.k0**2))
+Ek = (
+    16.0
+    * np.sqrt(2.0 / np.pi)
+    * (kmag ** 4)
+    / (args.k0 ** 5)
+    * np.exp(-2.0 * (kmag ** 2) / (args.k0 ** 2))
+)
 
 # Draw random numbers
 np.random.seed(args.seed)
@@ -137,7 +142,7 @@ phi2 = np.random.uniform(0, 2 * np.pi, np.shape(kmag))
 phi3 = np.random.uniform(0, 2 * np.pi, np.shape(kmag))
 
 # the random quantities
-prefix = np.sqrt(2.0 * div0(Ek, 4.0 * np.pi * (kmag**2)))
+prefix = np.sqrt(2.0 * div0(Ek, 4.0 * np.pi * (kmag ** 2)))
 a = prefix * np.exp(1j * phi1) * np.cos(phi3)
 b = prefix * np.exp(1j * phi2) * np.sin(phi3)
 
@@ -154,27 +159,30 @@ uf[N:halfN:-1, 0, 0] = np.conj(uf[1:halfN, 0, 0])
 # symmetry on first row
 uf[0, N:halfN:-1, 0] = np.conj(uf[0, 1:halfN, 0])
 # symmetry about the (halfN,halfN) element
-uf[halfN - 1:0:-1, N:halfN - 1:-1,
-    0] = np.conj(uf[halfN + 1:N, 1:halfN + 1, 0])
+uf[halfN - 1 : 0 : -1, N : halfN - 1 : -1, 0] = np.conj(
+    uf[halfN + 1 : N, 1 : halfN + 1, 0]
+)
 
 vf[N:halfN:-1, N:halfN:-1, 0] = np.conj(vf[1:halfN, 1:halfN, 0])
-vf[halfN - 1:0:-1, N:halfN - 1:-1,
-    0] = np.conj(vf[halfN + 1:N, 1:halfN + 1, 0])
+vf[halfN - 1 : 0 : -1, N : halfN - 1 : -1, 0] = np.conj(
+    vf[halfN + 1 : N, 1 : halfN + 1, 0]
+)
 vf[N:halfN:-1, 0, 0] = np.conj(vf[1:halfN, 0, 0])
 vf[0, N:halfN:-1, 0] = np.conj(vf[0, 1:halfN, 0])
 
 wf[N:halfN:-1, N:halfN:-1, 0] = np.conj(wf[1:halfN, 1:halfN, 0])
-wf[halfN - 1:0:-1, N:halfN - 1:-1,
-    0] = np.conj(wf[halfN + 1:N, 1:halfN + 1, 0])
+wf[halfN - 1 : 0 : -1, N : halfN - 1 : -1, 0] = np.conj(
+    wf[halfN + 1 : N, 1 : halfN + 1, 0]
+)
 wf[N:halfN:-1, 0, 0] = np.conj(wf[1:halfN, 0, 0])
 wf[0, N:halfN:-1, 0] = np.conj(wf[0, 1:halfN, 0])
 
 # Normalize. Because we are generating the data in wavenumber space,
 # we have to multiply by N**3 because in the definition of the numpy
 # ifftn there is a 1/N**n.
-uf = uf * N**3
-vf = vf * N**3
-wf = wf * N**3
+uf = uf * N ** 3
+vf = vf * N ** 3
+wf = wf * N ** 3
 
 # # Quick check on energy content (make sure you add both the current
 # # contribution and the one we are neglecting because we are assuming
@@ -205,8 +213,11 @@ v = np.fft.irfftn(vf, s=(N, N, N))
 w = np.fft.irfftn(wf, s=(N, N, N))
 
 # Another energy content check
-print('Energy = 1/V * int E(x,y,z) dV = 0.5/V * int (u**2 + v**2 + w**2) dx dy dz = {0:.10f} ~= 3/2'.format(
-    np.sum(u**2 + v**2 + w**2) * 0.5 * (dx / L)**3))
+print(
+    "Energy = 1/V * int E(x,y,z) dV = 0.5/V * int (u**2 + v**2 + w**2) dx dy dz = {0:.10f} ~= 3/2".format(
+        np.sum(u ** 2 + v ** 2 + w ** 2) * 0.5 * (dx / L) ** 3
+    )
+)
 
 # # Enstrophy check
 # _, dudy, dudz = np.gradient(u, dx)
@@ -222,29 +233,30 @@ print('Energy = 1/V * int E(x,y,z) dV = 0.5/V * int (u**2 + v**2 + w**2) dx dy d
 # 4. velocity fluctuations re-sampled on the desired grid (N^3)
 xr = np.linspace(xs, xe, args.N + 1)
 xrc = (xr[1:] + xr[:-1]) / 2
-Xr, Yr, Zr = np.meshgrid(xrc, xrc, xrc, indexing='ij')
+Xr, Yr, Zr = np.meshgrid(xrc, xrc, xrc, indexing="ij")
 
-Xr = Xr.reshape(-1, order='F')
-Yr = Yr.reshape(-1, order='F')
-Zr = Zr.reshape(-1, order='F')
+Xr = Xr.reshape(-1, order="F")
+Yr = Yr.reshape(-1, order="F")
+Zr = Zr.reshape(-1, order="F")
 
-ur = spi.interpn((xc, xc, xc), u, (Xr, Yr, Zr), method='linear')
-vr = spi.interpn((xc, xc, xc), v, (Xr, Yr, Zr), method='linear')
-wr = spi.interpn((xc, xc, xc), w, (Xr, Yr, Zr), method='linear')
+ur = spi.interpn((xc, xc, xc), u, (Xr, Yr, Zr), method="linear")
+vr = spi.interpn((xc, xc, xc), v, (Xr, Yr, Zr), method="linear")
+wr = spi.interpn((xc, xc, xc), w, (Xr, Yr, Zr), method="linear")
 
 
 # ========================================================================
 # Save the data in Fortran ordering
-fname = 'hit_ic_{0:d}_{1:d}.dat'.format(int(args.k0), args.N)
+fname = "hit_ic_{0:d}_{1:d}.dat".format(int(args.k0), args.N)
 data = np.vstack((Xr, Yr, Zr, ur, vr, wr)).T
-np.savetxt(fname, data, fmt='%.18e', delimiter=',', header='x, y, z, u, v, w')
+np.savetxt(fname, data, fmt="%.18e", delimiter=",", header="x, y, z, u, v, w")
 
 
 # ========================================================================
 # plot (only u fluctuations)
 if args.plot:
     import matplotlib as mpl
-    mpl.use('Agg')
+
+    mpl.use("Agg")
     import matplotlib.pyplot as plt
 
     datmin = u.min()
@@ -254,62 +266,113 @@ if args.plot:
     # Original data
     # transpose and origin change bc I used meshgrid with ij and not xy
     fig, ax = plt.subplots(nrows=3, ncols=3, figsize=(14, 14))
-    ax[0, 0].imshow(uo[:, :, 0].T, origin='lower', extent=[
-                    xs, xe, xs, xe], cmap='RdBu_r', vmin=datmin, vmax=datmax)
-    ax[0, 0].set_title('Original data (x,y)')
-    ax[0, 1].imshow(uo[:, 0, :].T, origin='lower', extent=[
-                    xs, xe, xs, xe], cmap='RdBu_r', vmin=datmin, vmax=datmax)
-    ax[0, 1].set_title('Original data (x,z)')
-    ax[0, 2].imshow(uo[0, :, :].T, origin='lower', extent=[
-                    xs, xe, xs, xe], cmap='RdBu_r', vmin=datmin, vmax=datmax)
-    ax[0, 2].set_title('Original data (y,z)')
+    ax[0, 0].imshow(
+        uo[:, :, 0].T,
+        origin="lower",
+        extent=[xs, xe, xs, xe],
+        cmap="RdBu_r",
+        vmin=datmin,
+        vmax=datmax,
+    )
+    ax[0, 0].set_title("Original data (x,y)")
+    ax[0, 1].imshow(
+        uo[:, 0, :].T,
+        origin="lower",
+        extent=[xs, xe, xs, xe],
+        cmap="RdBu_r",
+        vmin=datmin,
+        vmax=datmax,
+    )
+    ax[0, 1].set_title("Original data (x,z)")
+    ax[0, 2].imshow(
+        uo[0, :, :].T,
+        origin="lower",
+        extent=[xs, xe, xs, xe],
+        cmap="RdBu_r",
+        vmin=datmin,
+        vmax=datmax,
+    )
+    ax[0, 2].set_title("Original data (y,z)")
 
     # Filtered original data
-    ax[1, 0].imshow(u[:, :, 0].T, origin='lower', extent=[
-                    xs, xe, xs, xe], cmap='RdBu_r', vmin=datmin, vmax=datmax)
-    ax[1, 0].set_title('Filtered original data (x,y)')
-    ax[1, 1].imshow(u[:, 0, :].T, origin='lower', extent=[
-                    xs, xe, xs, xe], cmap='RdBu_r', vmin=datmin, vmax=datmax)
-    ax[1, 1].set_title('Filtered original data (x,z)')
-    ax[1, 2].imshow(u[0, :, :].T, origin='lower', extent=[
-                    xs, xe, xs, xe], cmap='RdBu_r', vmin=datmin, vmax=datmax)
-    ax[1, 2].set_title('Filtered original data (y,z)')
+    ax[1, 0].imshow(
+        u[:, :, 0].T,
+        origin="lower",
+        extent=[xs, xe, xs, xe],
+        cmap="RdBu_r",
+        vmin=datmin,
+        vmax=datmax,
+    )
+    ax[1, 0].set_title("Filtered original data (x,y)")
+    ax[1, 1].imshow(
+        u[:, 0, :].T,
+        origin="lower",
+        extent=[xs, xe, xs, xe],
+        cmap="RdBu_r",
+        vmin=datmin,
+        vmax=datmax,
+    )
+    ax[1, 1].set_title("Filtered original data (x,z)")
+    ax[1, 2].imshow(
+        u[0, :, :].T,
+        origin="lower",
+        extent=[xs, xe, xs, xe],
+        cmap="RdBu_r",
+        vmin=datmin,
+        vmax=datmax,
+    )
+    ax[1, 2].set_title("Filtered original data (y,z)")
 
     # Downsampled filtered data
-    ur = ur.reshape(args.N, args.N, args.N, order='F')
-    ax[2, 0].imshow(ur[:, :, 0].T, origin='lower', extent=[
-                    xs, xe, xs, xe], cmap='RdBu_r', vmin=datmin, vmax=datmax)
-    ax[2, 0].set_title('Downsampled data (x,y)')
-    ax[2, 1].imshow(ur[:, 0, :].T, origin='lower', extent=[
-                    xs, xe, xs, xe], cmap='RdBu_r', vmin=datmin, vmax=datmax)
-    ax[2, 1].set_title('Downsampled data (x,z)')
-    ax[2, 2].imshow(ur[0, :, :].T, origin='lower', extent=[
-                    xs, xe, xs, xe], cmap='RdBu_r', vmin=datmin, vmax=datmax)
-    ax[2, 2].set_title('Downsampled data (y,z)')
+    ur = ur.reshape(args.N, args.N, args.N, order="F")
+    ax[2, 0].imshow(
+        ur[:, :, 0].T,
+        origin="lower",
+        extent=[xs, xe, xs, xe],
+        cmap="RdBu_r",
+        vmin=datmin,
+        vmax=datmax,
+    )
+    ax[2, 0].set_title("Downsampled data (x,y)")
+    ax[2, 1].imshow(
+        ur[:, 0, :].T,
+        origin="lower",
+        extent=[xs, xe, xs, xe],
+        cmap="RdBu_r",
+        vmin=datmin,
+        vmax=datmax,
+    )
+    ax[2, 1].set_title("Downsampled data (x,z)")
+    ax[2, 2].imshow(
+        ur[0, :, :].T,
+        origin="lower",
+        extent=[xs, xe, xs, xe],
+        cmap="RdBu_r",
+        vmin=datmin,
+        vmax=datmax,
+    )
+    ax[2, 2].set_title("Downsampled data (y,z)")
 
-    plt.savefig('hit_ic_u_{0:d}_{1:d}.png'.format(
-        int(args.k0), args.N), format='png')
+    plt.savefig("hit_ic_u_{0:d}_{1:d}.png".format(int(args.k0), args.N), format="png")
 
     # Fourier coefficients of original data
     fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(14, 8))
-    ax[0, 0].imshow(np.real(uf[:, :, 0].T), origin='lower', cmap='RdBu_r')
-    ax[0, 0].set_title('Real Fourier coefficients (x,y)')
-    ax[0, 1].imshow(np.real(uf[:, 0, :].T), origin='lower', cmap='RdBu_r')
-    ax[0, 1].set_title('Real Fourier coefficients (x,z)')
-    ax[0, 2].imshow(np.real(uf[0, :, :].T), origin='lower', cmap='RdBu_r')
-    ax[0, 2].set_title('Real Fourier coefficients (y,z)')
-    ax[1, 0].imshow(np.imag(uf[:, :, 0].T), origin='lower', cmap='RdBu_r')
-    ax[1, 0].set_title('Imag Fourier coefficients (x,y)')
-    ax[1, 1].imshow(np.imag(uf[:, 0, :].T), origin='lower', cmap='RdBu_r')
-    ax[1, 1].set_title('Imag Fourier coefficients (x,z)')
-    ax[1, 2].imshow(np.imag(uf[0, :, :].T), origin='lower', cmap='RdBu_r')
-    ax[1, 2].set_title('Imag Fourier coefficients (y,z)')
-    plt.savefig('hit_ic_uf_{0:d}_{1:d}.png'.format(
-        int(args.k0), args.N), format='png')
+    ax[0, 0].imshow(np.real(uf[:, :, 0].T), origin="lower", cmap="RdBu_r")
+    ax[0, 0].set_title("Real Fourier coefficients (x,y)")
+    ax[0, 1].imshow(np.real(uf[:, 0, :].T), origin="lower", cmap="RdBu_r")
+    ax[0, 1].set_title("Real Fourier coefficients (x,z)")
+    ax[0, 2].imshow(np.real(uf[0, :, :].T), origin="lower", cmap="RdBu_r")
+    ax[0, 2].set_title("Real Fourier coefficients (y,z)")
+    ax[1, 0].imshow(np.imag(uf[:, :, 0].T), origin="lower", cmap="RdBu_r")
+    ax[1, 0].set_title("Imag Fourier coefficients (x,y)")
+    ax[1, 1].imshow(np.imag(uf[:, 0, :].T), origin="lower", cmap="RdBu_r")
+    ax[1, 1].set_title("Imag Fourier coefficients (x,z)")
+    ax[1, 2].imshow(np.imag(uf[0, :, :].T), origin="lower", cmap="RdBu_r")
+    ax[1, 2].set_title("Imag Fourier coefficients (y,z)")
+    plt.savefig("hit_ic_uf_{0:d}_{1:d}.png".format(int(args.k0), args.N), format="png")
 
     # plt.show()
 
 # output timer
 end = time.time() - start
-print("Elapsed time " + str(timedelta(seconds=end)) +
-      " (or {0:f} seconds)".format(end))
+print("Elapsed time " + str(timedelta(seconds=end)) + " (or {0:f} seconds)".format(end))
