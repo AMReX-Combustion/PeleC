4107aaa14f5bcb4311633266a42ba02372a8e038
Anne Felden <Enna@lbl.gov>
2019-03-14 13:52:01 -0700

    [DVODE/CVODE] Make chemistry integration compatible with new PelePhysics (branch cvode_C_merge)

--- Source/Src_nd/React_nd.F90
+++ Source/Src_nd/React_nd.F90
@@ -18,10 +18,9 @@ contains
     use network           , only : nspec
     use meth_params_module, only : NVAR, URHO, UMX, UMZ, UEDEN, UEINT, UTEMP, &
                                    UFS
-    use react_type_module
     use reactor_module, only : react
+    use amrex_fort_module, only : amrex_real
     use amrex_constants_module, only : HALF
-    use react_type_module
 
     implicit none
 
@@ -44,11 +43,9 @@ contains
     integer          :: i, j, k
     double precision :: rho_e_K_old,rho_e_K_new, rhoE_old, rhoE_new, rho_new, mom_new(3)
 
-    type (react_t) :: react_state_in, react_state_out
-    type (reaction_stat_t)  :: stat
+    real(amrex_real) ::    rY(nspec+1), rY_src(nspec)
+    real(amrex_real) ::    energy, energy_src, pressure, rho
 
-    call build(react_state_in)
-    call build(react_state_out)
 
     do k = lo(3), hi(3)
        do j = lo(2), hi(2)
@@ -58,27 +55,31 @@ contains
 
                 rhoE_old                        = uold(i,j,k,UEDEN)
                 rho_e_K_old                     = HALF * sum(uold(i,j,k,UMX:UMZ)**2) / uold(i,j,k,URHO)
-                react_state_in %            rho = sum(uold(i,j,k,UFS:UFS+nspec-1))
-                react_state_in %              e = (rhoE_old - rho_e_K_old) / uold(i,j,k,URHO)
-                react_state_in %              T = uold(i,j,k,UTEMP)
-                react_state_in %        rhoY(:) = uold(i,j,k,UFS:UFS+nspec-1)
-                !react_state_in %    rhoedot_ext = asrc(i,j,k,UEINT)
+                rho                             = sum(uold(i,j,k,UFS:UFS+nspec-1))
+
+                energy           = (rhoE_old - rho_e_K_old) / uold(i,j,k,URHO)
+                rY(nspec+1)      = uold(i,j,k,UTEMP)
+                rY(1:nspec)      = uold(i,j,k,UFS:UFS+nspec-1)
 
                 ! rho.e source term computed using (rho.E,rho.u,rho)_new rather than pulling from UEINT comp of asrc
                 rho_e_K_new = HALF * sum(unew(i,j,k,UMX:UMZ)**2) / unew(i,j,k,URHO)
-                react_state_in % rhoedot_ext = ( (unew(i,j,k,UEDEN) - rho_e_K_new) &
-                     -                           (react_state_in % rho  *  react_state_in % e) ) / dt_react
+                energy_src       = ( (unew(i,j,k,UEDEN) - rho_e_K_new) &
+                       -                (rho  *  energy) ) / dt_react
 
-                react_state_in % rhoYdot_ext(:) = asrc(i,j,k,UFS:UFS+nspec-1)
-                react_state_in % i = i
-                react_state_in % j = j
-                react_state_in % k = k
+                rY_src(1:nspec)  = asrc(i,j,k,UFS:UFS+nspec-1)
+                !react_state_in % i = i
+                !react_state_in % j = j
+                !react_state_in % k = k
 
-                stat = react(react_state_in, react_state_out, dt_react, time)
+                pressure         = 1013250.d0
 
-                rho_new = sum(react_state_out % rhoY(:))
+                cost(i,j,k) = react(rY, rY_src,&
+                                    energy, energy_src,&
+                                    pressure,dt_react,time,0)
+
+                rho_new = sum(rY(1:nspec))
                 mom_new = uold(i,j,k,UMX:UMZ) + dt_react*asrc(i,j,k,UMX:UMZ)
-                rhoe_new = rho_new  *  react_state_out % e
+                rhoe_new = rho_new  *  energy
                 rho_e_K_new = HALF * sum(mom_new**2) / rho_new
                 rhoE_new    = rhoe_new + rho_e_K_new
                 
@@ -88,8 +89,8 @@ contains
                    unew(i,j,k,UMX:UMZ)         = mom_new
                    unew(i,j,k,UEINT)           = rhoe_new
                    unew(i,j,k,UEDEN)           = rhoE_new
-                   unew(i,j,k,UTEMP)           = react_state_out % T
-                   unew(i,j,k,UFS:UFS+nspec-1) = react_state_out % rhoY(:)
+                   unew(i,j,k,UTEMP)           = rY(nspec+1)
+                   unew(i,j,k,UFS:UFS+nspec-1) = rY(1:nspec)
 
                 endif
 
@@ -101,21 +102,15 @@ contains
                      j .ge. IR_lo(2) .and. j .le. IR_hi(2) .and. &
                      k .ge. IR_lo(3) .and. k .le. IR_hi(3) ) then
 
-                   IR(i,j,k,1:nspec) = (react_state_out % rhoY(:) - react_state_in % rhoY(:)) / dt_react - asrc(i,j,k,UFS:UFS+nspec-1)
+                   IR(i,j,k,1:nspec) = (rY(1:nspec) - uold(i,j,k,UFS:UFS+nspec-1)) / dt_react - asrc(i,j,k,UFS:UFS+nspec-1)
                    IR(i,j,k,nspec+1) = (        rhoE_new          -         rhoE_old        ) / dt_react - asrc(i,j,k,UEDEN          )
 
                 endif
-
-                ! Record cost of reactions
-                cost(i,j,k) = stat % cost_value
 
              end if
           end do
        enddo
     enddo
-
-    call destroy(react_state_in)
-    call destroy(react_state_out)
 
   end subroutine pc_react_state
 
