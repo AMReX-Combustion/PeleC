ff762a3a2c0eba70a90ba117985f584efb399f16
Jon Rood <jon.rood@nrel.gov>
2019-05-06 16:15:43 -0600

    Adding sod-3d-1 test and adding consistent runtime options for all tests.

--- Testing/CTestList.cmake
+++ Testing/CTestList.cmake
@@ -28,13 +28,15 @@ function(add_test_r TEST_NAME NP)
     # Gather all files in source directory for test
     file(GLOB TEST_FILES "${CURRENT_TEST_SOURCE_DIR}/*")
     # Copy files to test working directory
-    file(COPY ${TEST_FILES} DESTINATION "${CURRENT_TEST_BINARY_DIR}/") 
+    file(COPY ${TEST_FILES} DESTINATION "${CURRENT_TEST_BINARY_DIR}/")
+    # Set some default runtime options for all tests in this category
+    set(RUNTIME_OPTIONS "max_step=10 amr.checkpoint_files_output=0 amr.plot_files_output=1 amr.probin_file=${TEST_NAME}.probin")
     # Build the exe for the test
     build_pelec(PeleC-${TEST_NAME} ${EXE_OPTIONS_FILE})
     # Place the exe in the correct working directory
     set_target_properties(PeleC-${TEST_NAME} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CURRENT_TEST_BINARY_DIR}/")
     # Add test and actual test commands to CTest database
-    add_test(${TEST_NAME} sh -c "${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${NP} ${MPIEXEC_PREFLAGS} ${CURRENT_TEST_BINARY_DIR}/PeleC-${TEST_NAME} ${MPIEXEC_POSTFLAGS} ${CURRENT_TEST_BINARY_DIR}/${TEST_NAME}.i && ${FCOMPARE} ${PLOT_GOLD} ${PLOT_TEST}")
+    add_test(${TEST_NAME} sh -c "${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${NP} ${MPIEXEC_PREFLAGS} ${CURRENT_TEST_BINARY_DIR}/PeleC-${TEST_NAME} ${MPIEXEC_POSTFLAGS} ${CURRENT_TEST_BINARY_DIR}/${TEST_NAME}.i ${RUNTIME_OPTIONS} && ${FCOMPARE} ${PLOT_GOLD} ${PLOT_TEST}")
     # Set properties for test
     set_tests_properties(${TEST_NAME} PROPERTIES TIMEOUT 500 PROCESSORS ${NP} WORKING_DIRECTORY "${CURRENT_TEST_BINARY_DIR}/" LABELS "regression")
 endfunction(add_test_r)
@@ -64,10 +66,11 @@ add_test_r(fiab-3d 4)
 add_test_r(hit-3d-1 4)
 add_test_r(hit-3d-2 4)
 add_test_r(hit-3d-3 4)
+#add_test_r(mms-3d-1 4)
+add_test_r(sod-3d-1 4)
 add_test_r(tg-2d-1 4)
 add_test_r(tg-3d-1 4)
 add_test_r(tg-3d-2 4)
-add_test_r(mms-3d-1 4)
 
 #=============================================================================
 # Verification tests
