f52acf3c493b3b45950b15ec560102696a4c5942
Jon Rood <jon.rood@nrel.gov>
2019-05-07 13:19:23 -0600

    Add first verification test.

--- Testing/CTestList.cmake
+++ Testing/CTestList.cmake
@@ -47,6 +47,27 @@ function(add_test_r TEST_NAME NP)
     set_tests_properties(${TEST_NAME} PROPERTIES TIMEOUT 500 PROCESSORS ${NP} WORKING_DIRECTORY "${CURRENT_TEST_BINARY_DIR}/" LABELS "regression")
 endfunction(add_test_r)
 
+# Standard verification test
+function(add_test_v TEST_NAME TEST_DEPENDENCY NP)
+    # Set variables for respective binary and source directories for the test
+    set(CURRENT_TEST_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/test_files/${TEST_NAME})
+    set(CURRENT_TEST_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/test_files/${TEST_NAME})
+    set(TEST_DEPENDENCY_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/test_files/${TEST_DEPENDENCY})
+    set(TEST_DEPENDENCY_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/test_files/${TEST_DEPENDENCY})
+    # Make working directory for test
+    file(MAKE_DIRECTORY ${CURRENT_TEST_BINARY_DIR})
+    # Gather all files in source directory for test
+    file(GLOB TEST_FILES "${CURRENT_TEST_SOURCE_DIR}/*")
+    # Copy files to test working directory
+    file(COPY ${TEST_FILES} DESTINATION "${CURRENT_TEST_BINARY_DIR}/")
+    # Set some default runtime options for all tests in this category
+    set(RUNTIME_OPTIONS "max_step=10 amr.checkpoint_files_output=0 amr.plot_files_output=1 amr.probin_file=${TEST_NAME}.probin")
+    # Add test and actual test commands to CTest database
+    add_test(${TEST_NAME} sh -c "${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${NP} ${MPIEXEC_PREFLAGS} ${TEST_DEPENDENCY_BINARY_DIR}/PeleC-${TEST_DEPENDENCY} ${MPIEXEC_POSTFLAGS} ${CURRENT_TEST_BINARY_DIR}/${TEST_NAME}.i ${RUNTIME_OPTIONS} ${FCOMPARE_COMMAND}")
+    # Set properties for test
+    set_tests_properties(${TEST_NAME} PROPERTIES TIMEOUT 500 PROCESSORS ${NP} WORKING_DIRECTORY "${CURRENT_TEST_BINARY_DIR}/" LABELS "verification" FIXTURES_REQUIRED ${TEST_DEPENDENCY})
+endfunction(add_test_v)
+
 # Standard unit test
 function(add_test_u TEST_NAME NP)
     # Set variables for respective binary and source directories for the test
@@ -67,26 +88,29 @@ endfunction(add_test_u)
 #=============================================================================
 # Regression tests
 #=============================================================================
-add_test_r(fiab-2d 4)
-add_test_r(fiab-3d 4)
-add_test_r(hit-3d-1 4)
-add_test_r(hit-3d-2 4)
-add_test_r(hit-3d-3 4)
-add_test_r(mms-1d-1 4)
-add_test_r(mms-2d-1 4)
-add_test_r(mms-2d-2 4)
+#add_test_r(fiab-2d 4)
+#add_test_r(fiab-3d 4)
+#add_test_r(hit-3d-1 4)
+#add_test_r(hit-3d-2 4)
+#add_test_r(hit-3d-3 4)
+#add_test_r(mms-1d-1 4)
+#add_test_r(mms-2d-1 4)
+#add_test_r(mms-2d-2 4)
 add_test_r(mms-3d-1 4)
-add_test_r(mms-3d-2 4)
-add_test_r(mms-3d-3 4)
-add_test_r(mms-3d-4 1)
-add_test_r(sod-3d-1 4)
-add_test_r(tg-2d-1 4)
-add_test_r(tg-3d-1 4)
-add_test_r(tg-3d-2 4)
+#add_test_r(mms-3d-2 4)
+#add_test_r(mms-3d-3 4)
+#add_test_r(mms-3d-4 1)
+#add_test_r(sod-3d-1 4)
+#add_test_r(tg-2d-1 4)
+#add_test_r(tg-3d-1 4)
+#add_test_r(tg-3d-2 4)
 
 #=============================================================================
 # Verification tests
 #=============================================================================
+if(ENABLE_VERIFICATION)
+  add_test_v(symmetry_3d mms-3d-1 4)
+endif()
 
 #=============================================================================
 # Unit tests
