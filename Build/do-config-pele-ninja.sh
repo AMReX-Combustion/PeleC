c88085ecc58b635112018a81e5106bb3fcdf8ec2
Jon Rood <jon.rood@nrel.gov>
2019-05-08 16:54:03 -0600

    Update example config scripts.

--- Build/do-config-pele-ninja.sh
+++ Build/do-config-pele-ninja.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 # Example CMake config script for and OSX laptop with OpenMPI
-#export PATH=$PATH:$(spack location -i ninja)/bin # or whatever to git the kitware version of ninja with fortran in your path
+
+#export PATH=$PATH:$(spack location -i ninja)/bin # or whatever to get the kitware version of ninja with fortran in your path
+
 cmake -G Ninja -DCMAKE_INSTALL_PREFIX:PATH=./install \
                -DCMAKE_BUILD_TYPE:STRING=Release \
                -DPELEC_ENABLE_MPI:BOOL=ON \
                -DCMAKE_CXX_COMPILER:STRING=mpicxx \
                -DCMAKE_C_COMPILER:STRING=mpicc \
                -DCMAKE_Fortran_COMPILER:STRING=mpifort \
-               -DMPI_CXX_COMPILER:STRING=mpicxx \
-               -DMPI_C_COMPILER:STRING=mpicc \
-               -DMPI_Fortran_COMPILER:STRING=mpifort \
                .. && ninja -j8
 
 # Extra options
-#      -DPELEC_ENABLE_MASA:BOOL=ON \
-#      -DMASA_DIR:STRING=$(spack location -i masa) \
-#      -DMPIEXEC_PREFLAGS:STRING=--oversubscribe \
-#      -DENABLE_TESTS:BOOL=ON \
-#      -DENABLE_DOCUMENTATION:BOOL=ON \
-#      -DENABLE_FCOMPARE:BOOL=ON \
+      #-DENABLE_FCOMPARE:BOOL=ON \
+      #-DENABLE_TESTS:BOOL=ON \
+      #-DENABLE_VERIFICATION:BOOL=ON \
+      #-DTEST_WITH_FCOMPARE:BOOL=OFF \
+      #-DPELEC_ENABLE_MASA:BOOL=ON \
+      #-DMASA_DIR:STRING=$(spack location -i masa) \
+      #-DMPIEXEC_PREFLAGS:STRING=--oversubscribe \
+      #-DENABLE_DOCUMENTATION:BOOL=OFF \
