c60e112795bc556e2023f9cc495c9ac9f876972f
Ray Grout <ray.grout@nrel.gov>
2019-05-15 14:42:05 -0700

    fixed messed up comment

--- Source/PeleC_init_eb.cpp
+++ Source/PeleC_init_eb.cpp
@@ -172,7 +172,7 @@ PeleC::initialize_eb2_structs() {
                                    &Ncut, &dx);
       } else if (bgs == 1) {
         amrex::Print() << "This gradient stencil type WIP and not functional!" << bgs << std::endl;
-        amrex::Abort();Unknown or unspecified boundary gradient stencil type
+        amrex::Abort();
         pc_fill_bndry_grad_stencil_amrex(BL_TO_FORTRAN_BOX(tbox),
                                          sv_eb_bndry_geom[iLocal].data(), &Ncut,
                                          sv_eb_bndry_grad_stencil[iLocal].data(),
@@ -180,7 +180,7 @@ PeleC::initialize_eb2_structs() {
 
       } else if (bgs == 2) {
         amrex::Print() << "This gradient stencil type WIP and not functional!" << bgs << std::endl;
-        amrex::Abort();Unknown or unspecified boundary gradient stencil type
+        amrex::Abort();
         pc_fill_bndry_grad_stencil_ls(BL_TO_FORTRAN_BOX(tbox),
                                       sv_eb_bndry_geom[iLocal].data(), &Ncut,
                                       sv_eb_bndry_grad_stencil[iLocal].data(),
