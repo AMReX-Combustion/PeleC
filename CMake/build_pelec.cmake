f3905cfd2e068c4de3d5d698debcfc8de1c0a75b
Jon Rood <jon.rood@nrel.gov>
2019-05-17 15:13:00 -0600

    Only build the single necessary library for a single executable.

--- CMake/build_pelec.cmake
+++ CMake/build_pelec.cmake
@@ -87,9 +87,10 @@ function(build_pelec pelec_exe_name pelec_exe_options_file)
   target_compile_definitions(${pelec_exe_name} PRIVATE $<$<COMPILE_LANGUAGE:Fortran>:BL_LANG_FORT>)
   target_compile_definitions(${pelec_exe_name} PRIVATE $<$<COMPILE_LANGUAGE:Fortran>:AMREX_LANG_FORT>)
 
-  if(${CMAKE_BUILD_TYPE} MATCHES "Release")
-    target_compile_definitions(${pelec_exe_name} PRIVATE NDEBUG)
-  endif()
+  # CMake BUILD_TYPE should already define this
+  #if(${CMAKE_BUILD_TYPE} MATCHES "Release")
+  #  target_compile_definitions(${pelec_exe_name} PRIVATE NDEBUG)
+  #endif()
 
   if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     target_compile_definitions(${pelec_exe_name} PRIVATE BL_Darwin)
