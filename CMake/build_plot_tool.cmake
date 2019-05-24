3ad05fb0f11356d3ebc95301cf410b00717b024c
Jon Rood <jon.rood@nrel.gov>
2019-05-24 13:29:19 -0600

    Test with fextrema (#83)
    * Add ability to build fextrema.
    
    * Output data from fextrema to file.
    
    * Add fextrema gold files and command for testing with fextrema.
    
    * Enable testing with fextrema in Travis.
    
    * Rather than run ctest multiple times, we just exclude longer running tests.
    
    * Fix regex for excluding tests.

---
+++ CMake/build_plot_tool.cmake
@@ -0,0 +1,56 @@
+function(build_plot_tool PLOT_TOOL_NAME)
+
+  #Expose functions we want to be able to call
+  include(${CMAKE_SOURCE_DIR}/CMake/add_source_function.cmake)
+  include(${CMAKE_SOURCE_DIR}/CMake/plot_tool_sources.cmake)
+
+  #Clear source file list from any previous executables
+  set_property(GLOBAL PROPERTY GlobalPlotToolSourceList "") 
+
+  #Aggregate amrex and pelephysics source files
+  get_plot_tool_sources(${PLOT_TOOL_NAME})
+  
+  #Put source list from global property into local list
+  get_property(PLOT_TOOL_SOURCES GLOBAL PROPERTY GlobalPlotToolSourceList)
+
+  #Create an executable based on all the source files we aggregated
+  add_executable(${PLOT_TOOL_NAME} ${PLOT_TOOL_SOURCES})
+
+  #Set definitions for our particular executable
+  target_compile_definitions(${PLOT_TOOL_NAME} PRIVATE BL_SPACEDIM=3)
+  target_compile_definitions(${PLOT_TOOL_NAME} PRIVATE BL_FORT_USE_UNDERSCORE)
+  target_compile_definitions(${PLOT_TOOL_NAME} PRIVATE AMREX_SPACEDIM=3)
+  target_compile_definitions(${PLOT_TOOL_NAME} PRIVATE AMREX_FORT_USE_UNDERSCORE)
+  target_compile_definitions(${PLOT_TOOL_NAME} PRIVATE $<$<COMPILE_LANGUAGE:Fortran>:BL_LANG_FORT>)
+  target_compile_definitions(${PLOT_TOOL_NAME} PRIVATE $<$<COMPILE_LANGUAGE:Fortran>:AMREX_LANG_FORT>)
+  # CMake BUILD_TYPE should already define this for us
+  #if(${CMAKE_BUILD_TYPE} MATCHES "Release")
+  #  target_compile_definitions(${PLOT_TOOL_NAME} PRIVATE NDEBUG)
+  #endif()
+  if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+    target_compile_definitions(${PLOT_TOOL_NAME} PRIVATE BL_Darwin)
+    target_compile_definitions(${PLOT_TOOL_NAME} PRIVATE AMREX_Darwin)
+  endif()
+  
+  #AMReX include directories
+  target_include_directories(${PLOT_TOOL_NAME} SYSTEM PRIVATE ${CMAKE_SOURCE_DIR}/Submodules/AMReX/Src/Base)
+  target_include_directories(${PLOT_TOOL_NAME} SYSTEM PRIVATE ${CMAKE_SOURCE_DIR}/Submodules/AMReX/Tools/Plotfile)
+  
+  #Link our executable to the MPI libraries, etc
+  #if(PELEC_ENABLE_MPI)
+  #  target_link_libraries(${PLOT_TOOL_NAME} PRIVATE MPI::MPI_CXX MPI::MPI_C MPI::MPI_Fortran)
+  #  target_compile_definitions(${PLOT_TOOL_NAME} PRIVATE BL_USE_MPI)
+  #  target_compile_definitions(${PLOT_TOOL_NAME} PRIVATE AMREX_USE_MPI)
+  #endif()
+ 
+  #Keep our Fortran module files confined to a unique directory for each executable 
+  set_target_properties(${PLOT_TOOL_NAME} PROPERTIES Fortran_MODULE_DIRECTORY
+                       "${CMAKE_BINARY_DIR}/fortran_modules/${PLOT_TOOL_NAME}_fortran_modules")
+
+  #Define what we want to be installed during a make install 
+  install(TARGETS ${PLOT_TOOL_NAME}
+          RUNTIME DESTINATION bin
+          ARCHIVE DESTINATION lib
+          LIBRARY DESTINATION lib)
+  
+endfunction(build_plot_tool PLOT_TOOL_NAME)
--- Testing/CTestList.cmake
+++ Testing/CTestList.cmake
@@ -3,15 +3,44 @@ include(${CMAKE_SOURCE_DIR}/CMake/build_unit_test.cmake)
 include(${CMAKE_SOURCE_DIR}/CMake/build_pelec.cmake)
 
 # Set location of gold files according to system/compiler/compiler_version
-set(GOLD_FILES_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/PeleCGoldFiles/${CMAKE_SYSTEM_NAME}/${CMAKE_CXX_COMPILER_ID}/${CMAKE_CXX_COMPILER_VERSION})
+set(FCOMPARE_GOLD_FILES_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/PeleCGoldFiles/${CMAKE_SYSTEM_NAME}/${CMAKE_CXX_COMPILER_ID}/${CMAKE_CXX_COMPILER_VERSION})
 
 if(TEST_WITH_FCOMPARE)
-  message("-- Test golds directory: ${GOLD_FILES_DIRECTORY}")
+  message(STATUS "Test golds directory for fcompare: ${FCOMPARE_GOLD_FILES_DIRECTORY}")
 endif()
 
 # Have CMake discover the number of cores on the node
 include(ProcessorCount)
 ProcessorCount(PROCESSES)
+
+# Set TOLERANCE for testing
+if(NOT ${TEST_TOLERANCE} STREQUAL "")
+  set(TOLERANCE ${TEST_TOLERANCE}) # User defined
+else(NOT ${TEST_TOLERANCE} STREQUAL "")
+  if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+    if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang"
+        OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
+      set(TOLERANCE "1e-3")
+    elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+      set(TOLERANCE "1e-3")
+    elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
+      set(TOLERANCE "1e-2")
+    else()
+      set(TOLERANCE "1e-8") # Mac default
+    endif()
+  elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+    if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+      set(TOLERANCE "1e-5")
+    elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+      set(TOLERANCE "1e-15")
+    elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
+      set(TOLERANCE "1e-2")
+    else()
+      set(TOLERANCE "1e-8") # Linux default
+    endif()
+  endif()
+endif()
+message(STATUS "Using test tolerance of ${TOLERANCE}")
 
 #=============================================================================
 # Functions for adding tests / Categories of tests
@@ -23,7 +52,7 @@ function(add_test_r TEST_NAME NP)
     set(CURRENT_TEST_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/test_files/${TEST_NAME})
     set(CURRENT_TEST_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/test_files/${TEST_NAME})
     # Gold files should be submodule organized by machine and compiler (these are output during configure)
-    set(PLOT_GOLD ${GOLD_FILES_DIRECTORY}/${TEST_NAME}/plt00010)
+    set(PLOT_GOLD ${FCOMPARE_GOLD_FILES_DIRECTORY}/${TEST_NAME}/plt00010)
     # Test plot is currently expected to be after 10 steps
     set(PLOT_TEST ${CURRENT_TEST_BINARY_DIR}/plt00010)
     # Get test options
@@ -31,7 +60,13 @@ function(add_test_r TEST_NAME NP)
     # Define our test options
     include(${EXE_OPTIONS_FILE})
     # Find fcompare
-    set(FCOMPARE ${CMAKE_BINARY_DIR}/fcompare)
+    if(TEST_WITH_FCOMPARE)
+      set(FCOMPARE ${CMAKE_BINARY_DIR}/fcompare)
+    endif()
+    # Find fextrema
+    if(TEST_WITH_FEXTREMA)
+      set(FEXTREMA ${CMAKE_BINARY_DIR}/fextrema)
+    endif()
     # Make working directory for test
     file(MAKE_DIRECTORY ${CURRENT_TEST_BINARY_DIR})
     # Gather all files in source directory for test
@@ -42,14 +77,18 @@ function(add_test_r TEST_NAME NP)
     set(RUNTIME_OPTIONS "max_step=10 amr.plot_file=plt amr.checkpoint_files_output=0 amr.plot_files_output=1 amr.probin_file=${TEST_NAME}.probin")
     # Build the exe for the test
     build_pelec(PeleC-${TEST_NAME} ${EXE_OPTIONS_FILE})
-    # Either just run the tests, or also use fcompare to test diffs in plots against gold files
+    # Use fcompare to test diffs in plots against gold files
     if(TEST_WITH_FCOMPARE)
       set(FCOMPARE_COMMAND "&& ${FCOMPARE} ${PLOT_GOLD} ${PLOT_TEST}")
+    endif()
+    # Use fextrema to test diffs in plots against gold files
+    if(TEST_WITH_FEXTREMA)
+      set(FEXTREMA_COMMAND "&& ${FEXTREMA} ${PLOT_TEST} > ${CURRENT_TEST_BINARY_DIR}/${TEST_NAME}.ext && ${PYTHON_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/test_files/fextrema_compare.py -f ${CURRENT_TEST_BINARY_DIR}/${TEST_NAME}.ext -g ${CURRENT_TEST_SOURCE_DIR}/${TEST_NAME}.ext.gold -t ${TOLERANCE}")
     endif()
     # Place the exe in the correct working directory
     set_target_properties(PeleC-${TEST_NAME} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CURRENT_TEST_BINARY_DIR}/")
     # Add test and actual test commands to CTest database
-    add_test(${TEST_NAME} sh -c "${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${NP} ${MPIEXEC_PREFLAGS} ${CURRENT_TEST_BINARY_DIR}/PeleC-${TEST_NAME} ${MPIEXEC_POSTFLAGS} ${CURRENT_TEST_BINARY_DIR}/${TEST_NAME}.i ${RUNTIME_OPTIONS} ${FCOMPARE_COMMAND}")
+    add_test(${TEST_NAME} sh -c "${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${NP} ${MPIEXEC_PREFLAGS} ${CURRENT_TEST_BINARY_DIR}/PeleC-${TEST_NAME} ${MPIEXEC_POSTFLAGS} ${CURRENT_TEST_BINARY_DIR}/${TEST_NAME}.i ${RUNTIME_OPTIONS} ${FEXTREMA_COMMAND} ${FCOMPARE_COMMAND}")
     # Set properties for test
     set_tests_properties(${TEST_NAME} PROPERTIES TIMEOUT 500 PROCESSORS ${NP} WORKING_DIRECTORY "${CURRENT_TEST_BINARY_DIR}/" LABELS "regression")
 endfunction(add_test_r)
