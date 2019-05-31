3c0b041a32a8710c92ac5fcdd058bf183ab68d05
Jon Rood <jon.rood@nrel.gov>
2019-05-31 16:44:49 -0600

    Default the GNU make system to use the submodules for AMReX and PelePhysics by default. Also update documentation regarding this and add some further documentation.

--- Build/do-config-pele.sh
+++ Build/do-config-pele.sh
@@ -8,14 +8,16 @@ cmake -DCMAKE_INSTALL_PREFIX:PATH=./install \
       -DCMAKE_CXX_COMPILER:STRING=mpicxx \
       -DCMAKE_C_COMPILER:STRING=mpicc \
       -DCMAKE_Fortran_COMPILER:STRING=mpifort \
+      -DENABLE_DOCUMENTATION:BOOL=ON \
       ..
 
 # Extra options
       #-DENABLE_FCOMPARE:BOOL=ON \
+      #-DENABLE_FEXTREMA:BOOL=ON \
       #-DENABLE_TESTS:BOOL=ON \
       #-DENABLE_VERIFICATION:BOOL=ON \
       #-DTEST_WITH_FCOMPARE:BOOL=OFF \
+      #-DTEST_WITH_FEXTREMA:BOOL=OFF \
       #-DPELEC_ENABLE_MASA:BOOL=ON \
       #-DMASA_DIR:STRING=$(spack location -i masa) \
       #-DMPIEXEC_PREFLAGS:STRING=--oversubscribe \
-      #-DENABLE_DOCUMENTATION:BOOL=OFF \
