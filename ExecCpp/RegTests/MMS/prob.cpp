ca45bb1f28e29265de68412be2c713ed332ef43d
Jon Rood <jon.rood@nrel.gov>
2020-06-17 15:25:44 -0600

    Changes to account for 2D in MMS and Sedov. Also error out if trying to use EB in anything except 3D.

--- ExecCpp/RegTests/MMS/prob.cpp
+++ ExecCpp/RegTests/MMS/prob.cpp
@@ -101,9 +101,11 @@ amrex_probinit(
   pp.query("a_pz", ProbParm::a_pz);
 
   // Define the length scale
-  ProbParm::L_x = probhi[0] - problo[0];
-  ProbParm::L_y = probhi[1] - problo[1];
+  AMREX_D_TERM(
+  ProbParm::L_x = probhi[0] - problo[0];,
+  ProbParm::L_y = probhi[1] - problo[1];,
   ProbParm::L_z = probhi[2] - problo[2];
+  )
 
   // Initial density, velocity, and material properties
   amrex::Real eint, cs, cp;
