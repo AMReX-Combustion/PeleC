01c1b9efc41e5b2de11341119155ed8e9200e0fa
Jon Rood <jon.rood@nrel.gov>
2020-12-11 10:15:50 -0700

    Guard old compilers from using new warning flags.

--- CMake/SetPeleCCompileFlags.cmake
+++ CMake/SetPeleCCompileFlags.cmake
@@ -1,22 +1,22 @@
-# Logic for handling warnings
 if(PELEC_ENABLE_ALL_WARNINGS)
-  # GCC, Clang, and Intel seem to accept these
-  list(APPEND PELEC_CXX_FLAGS "-Wall" "-Wextra" "-pedantic"
-                              "-Wno-sign-compare"
-                              "-Wunreachable-code"
-                              "-Wnull-dereference"
-                              "-Wfloat-conversion"
-                              "-Wshadow"
-                              "-Woverloaded-virtual")
-  if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
+  list(APPEND PELEC_CXX_FLAGS "-Wall" "-Wextra" "-pedantic")
+  if(CMAKE_CXX_COMPILER_ID MATCHES "^(GNU|Clang|AppleClang)$")
+    if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 7.0)
+      list(APPEND PELEC_CXX_FLAGS "-faligned-new"
+                                  "-Wunreachable-code"
+                                  "-Wnull-dereference"
+                                  "-Wfloat-conversion"
+                                  "-Wshadow"
+                                  "-Woverloaded-virtual")
+    endif()
+  endif()
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
     list(APPEND PELEC_CXX_FLAGS "-diag-disable:11074,11076,10397,15335")
   endif()
-  if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 7.0)
-    # Avoid notes about -faligned-new with GCC > 7
-    list(APPEND PELEC_CXX_FLAGS "-faligned-new")
-  endif()
-  if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang" AND CMAKE_CXX_COMPILER_VERSION VERSION_EQUAL 9.0.0.9000037)
-    list(APPEND PELEC_CXX_FLAGS "-Wno-missing-braces")
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+    if(CMAKE_CXX_COMPILER_VERSION VERSION_EQUAL 9.0.0.9000037)
+      list(APPEND PELEC_CXX_FLAGS "-Wno-missing-braces")
+    endif()
   endif()
 endif()
 
@@ -24,6 +24,5 @@ if(PELEC_ENABLE_WERROR)
   list(APPEND PELEC_CXX_FLAGS "-Werror")
 endif()
 
-# Add our extra flags according to language
 separate_arguments(PELEC_CXX_FLAGS)
 target_compile_options(${pelec_exe_name} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:${PELEC_CXX_FLAGS}>)
