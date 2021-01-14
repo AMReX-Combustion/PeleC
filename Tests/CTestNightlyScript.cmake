c3a14220442df27773a7850f772f4bf7e529734c
Jon Rood <jon.rood@nrel.gov>
2021-01-14 16:52:18 -0700

    Allow nightly script to test latest AMReX.

--- Tests/CTestNightlyScript.cmake
+++ Tests/CTestNightlyScript.cmake
@@ -58,6 +58,25 @@ ctest_start("Nightly" TRACK "Nightly")
 message("\n -- Update - ${CTEST_BUILD_NAME} --")
 ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}" RETURN_VALUE result)
 message(" -- Update exit code = ${result} --")
+
+if(USE_LATEST_AMREX)
+  # ctest_update always performs a submodule update so we need to checkout AMReX here ourselves.
+  # A single execute_process command pipes output from one command to another so we use multiple.
+  message("\n -- Update AMReX- ${CTEST_BUILD_NAME} --")
+  execute_process (
+    COMMAND ${CTEST_GIT_COMMAND} checkout development
+    WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}/Submodules/AMReX
+  )
+  execute_process (
+    COMMAND ${CTEST_GIT_COMMAND} pull origin development
+    WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}/Submodules/AMReX
+  )
+  execute_process (
+    COMMAND ${CTEST_GIT_COMMAND} log -1 --pretty=oneline
+    WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}/Submodules/AMReX
+  )
+endif()
+
 if(result GREATER -1)
   message("\n -- Configure - ${CTEST_BUILD_NAME} --")
   ctest_configure(BUILD "${CTEST_BINARY_DIRECTORY}" RETURN_VALUE result)
