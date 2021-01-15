d8ccfdc05ddfef15f9a539359a291e38beae74cb
Jon Rood <jon.rood@nrel.gov>
2021-01-14 20:12:43 -0700

    Fix linking to MPI for plot file tools.

--- CMake/BuildPeleCExe.cmake
+++ CMake/BuildPeleCExe.cmake
@@ -171,7 +171,7 @@ function(build_pelec_exe pelec_exe_name)
   
   include(AMReXBuildInfo)
   generate_buildinfo(${pelec_exe_name} ${CMAKE_SOURCE_DIR})
-  target_include_directories(${pelec_exe_name} PUBLIC ${AMREX_SUBMOD_LOCATION}/Tools/C_scripts)
+  target_include_directories(${pelec_exe_name} PRIVATE ${AMREX_SUBMOD_LOCATION}/Tools/C_scripts)
 
   if(PELEC_ENABLE_MASA)
     target_compile_definitions(${pelec_exe_name} PRIVATE PELEC_USE_MASA)
@@ -180,7 +180,7 @@ function(build_pelec_exe pelec_exe_name)
   endif()
 
   if(PELEC_ENABLE_MPI)
-    target_link_libraries(${pelec_exe_name} PUBLIC $<$<BOOL:${MPI_CXX_FOUND}>:MPI::MPI_CXX>)
+    target_link_libraries(${pelec_exe_name} PRIVATE $<$<BOOL:${MPI_CXX_FOUND}>:MPI::MPI_CXX>)
   endif()
 
   #PeleC include directories
@@ -188,7 +188,7 @@ function(build_pelec_exe pelec_exe_name)
   target_include_directories(${pelec_exe_name} PRIVATE ${CMAKE_BINARY_DIR})
 
   #Link to amrex library
-  target_link_libraries(${pelec_exe_name} PRIVATE amrex)
+  target_link_libraries(${pelec_exe_name} PRIVATE AMReX::amrex)
 
   if(PELEC_ENABLE_CUDA)
     set(pctargets "${pelec_exe_name}")
