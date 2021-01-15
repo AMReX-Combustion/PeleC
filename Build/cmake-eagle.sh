70b357008a77c4619085faad915773a12e0e803f
Jon Rood <jon.rood@nrel.gov>
2021-01-14 21:03:45 -0700

    Update example Eagle configure script.

--- Build/cmake-eagle.sh
+++ Build/cmake-eagle.sh
@@ -2,18 +2,16 @@
 
 # Example CMake config script for running test suite on Eagle:
 
-module load mpt cuda cmake
+module load mpt cuda/10.2.89 cmake
 
 cmake -DCMAKE_INSTALL_PREFIX:PATH=./install \
-      -DCMAKE_CXX_COMPILER:STRING=mpicxx \
-      -DCMAKE_C_COMPILER:STRING=mpicc \
       -DCMAKE_BUILD_TYPE:STRING=Release \
       -DPELEC_DIM:STRING=3 \
       -DPELEC_ENABLE_AMREX_EB:BOOL=ON \
       -DPELEC_ENABLE_MPI:BOOL=ON \
       -DPELEC_ENABLE_TESTS:BOOL=ON \
-      -DPELEC_ENABLE_FCOMPARE:BOOL=OFF \
-      -DPELEC_ENABLE_FCOMPARE_FOR_TESTS:BOOL=OFF \
+      -DPELEC_ENABLE_FCOMPARE:BOOL=ON \
+      -DPELEC_ENABLE_FCOMPARE_FOR_TESTS:BOOL=ON \
       -DPELEC_ENABLE_SUNDIALS:BOOL=ON \
       -DPELEC_ENABLE_MASA:BOOL=OFF \
       -DPELEC_ENABLE_ALL_WARNINGS:BOOL=ON \
@@ -22,7 +20,7 @@ cmake -DCMAKE_INSTALL_PREFIX:PATH=./install \
       -DPELEC_ENABLE_CUDA:BOOL=ON \
       -DGPUS_PER_NODE:STRING=2 \
       -DGPUS_PER_SOCKET:STRING=1 \
-      -DAMReX_CUDA_ARCH=Volta \
+      -DAMReX_CUDA_ARCH:STRING=7.0 \
       -DPYTHON_EXECUTABLE=$(which python3) \
       -DPELEC_PRECISION:STRING=DOUBLE \
       .. 
