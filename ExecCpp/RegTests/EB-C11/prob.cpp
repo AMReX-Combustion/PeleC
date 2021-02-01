0232f866f06ce50b3b7c0cc0404f24951ad6f55f
Marc T. Henry de Frahan <marc.henrydefrahan@nrel.gov>
2021-02-01 11:00:56 -0700

    Remove more managed (#234)
    Removed more global managed variables from problem directories.

--- ExecCpp/RegTests/EB-C11/prob.cpp
+++ ExecCpp/RegTests/EB-C11/prob.cpp
@@ -1,26 +1,5 @@
 #include "prob.H"
 
-namespace ProbParm {
-AMREX_GPU_DEVICE_MANAGED amrex::Real p_l = 1.0;   // left pressure (erg/cc)
-AMREX_GPU_DEVICE_MANAGED amrex::Real u_l = 0.0;   // left velocity (cm/s)
-AMREX_GPU_DEVICE_MANAGED amrex::Real rho_l = 0.0; // left density (g/cc)
-AMREX_GPU_DEVICE_MANAGED amrex::Real rhoe_l;
-AMREX_GPU_DEVICE_MANAGED amrex::Real T_l = 1.0;
-AMREX_GPU_DEVICE_MANAGED amrex::Real p_r = 0.1;     // right pressure (erg/cc)
-AMREX_GPU_DEVICE_MANAGED amrex::Real u_r = 0.0;     // right velocity (cm/s)
-AMREX_GPU_DEVICE_MANAGED amrex::Real rho_r = 0.125; // right density (g/cc)
-AMREX_GPU_DEVICE_MANAGED amrex::Real rhoe_r;
-AMREX_GPU_DEVICE_MANAGED amrex::Real T_r = 1.0;
-AMREX_GPU_DEVICE_MANAGED amrex::Real frac =
-  0.5; // fraction of the domain for the interface
-AMREX_GPU_DEVICE_MANAGED int idir = 1; // direction across which to jump
-AMREX_GPU_DEVICE_MANAGED amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> split;
-AMREX_GPU_DEVICE_MANAGED int left_gas_id = N2_ID;
-AMREX_GPU_DEVICE_MANAGED int right_gas_id = HE_ID;
-std::string gasL = "N2";
-std::string gasR = "HE";
-} // namespace ProbParm
-
 void
 pc_prob_close()
 {
@@ -37,43 +16,47 @@ amrex_probinit(
 {
   // Parse params
   amrex::ParmParse pp("prob");
-  pp.query("p_l", ProbParm::p_l);
-  pp.query("u_l", ProbParm::u_l);
-  pp.query("rho_l", ProbParm::rho_l);
-  pp.query("p_r", ProbParm::p_r);
-  pp.query("u_r", ProbParm::u_r);
-  pp.query("rho_r", ProbParm::rho_r);
-  pp.query("frac", ProbParm::frac);
-  pp.query("idir", ProbParm::idir);
-  pp.get("left_gas", ProbParm::gasL);
-  pp.get("right_gas", ProbParm::gasR);
+  pp.query("p_l", PeleC::prob_parm_device->p_l);
+  pp.query("u_l", PeleC::prob_parm_device->u_l);
+  pp.query("rho_l", PeleC::prob_parm_device->rho_l);
+  pp.query("p_r", PeleC::prob_parm_device->p_r);
+  pp.query("u_r", PeleC::prob_parm_device->u_r);
+  pp.query("rho_r", PeleC::prob_parm_device->rho_r);
+  pp.query("frac", PeleC::prob_parm_device->frac);
+  pp.query("idir", PeleC::prob_parm_device->idir);
+  pp.get("left_gas", PeleC::prob_parm_device->gasL);
+  pp.get("right_gas", PeleC::prob_parm_device->gasR);
 
   for (int idir = 0; idir < AMREX_SPACEDIM; idir++) {
-    ProbParm::split[idir] = 0.5;
+    PeleC::prob_parm_device->split[idir] = 0.5;
   }
 
-  if (ProbParm::gasL == "N2") {
-    ProbParm::left_gas_id = N2_ID;
-    ProbParm::right_gas_id = HE_ID;
+  if (PeleC::prob_parm_device->gasL == "N2") {
+    PeleC::prob_parm_device->left_gas_id = N2_ID;
+    PeleC::prob_parm_device->right_gas_id = HE_ID;
   } else {
-    ProbParm::left_gas_id = HE_ID;
-    ProbParm::right_gas_id = N2_ID;
+    PeleC::prob_parm_device->left_gas_id = HE_ID;
+    PeleC::prob_parm_device->right_gas_id = N2_ID;
   }
 
   amrex::Real e_l;
   amrex::Real e_r /*, cs, cp*/;
   amrex::Real massfrac_l[NUM_SPECIES] = {0.0};
   amrex::Real massfrac_r[NUM_SPECIES] = {0.0};
-  massfrac_l[ProbParm::left_gas_id] = 1.0;
-  massfrac_r[ProbParm::right_gas_id] = 1.0;
+  massfrac_l[PeleC::prob_parm_device->left_gas_id] = 1.0;
+  massfrac_r[PeleC::prob_parm_device->right_gas_id] = 1.0;
 
-  EOS::RYP2E(ProbParm::rho_l, massfrac_l, ProbParm::p_l, e_l);
-  EOS::EY2T(e_l, massfrac_l, ProbParm::T_l);
-  ProbParm::rhoe_l = ProbParm::rho_l * e_l;
+  EOS::RYP2E(
+    PeleC::prob_parm_device->rho_l, massfrac_l, PeleC::prob_parm_device->p_l,
+    e_l);
+  EOS::EY2T(e_l, massfrac_l, PeleC::prob_parm_device->T_l);
+  PeleC::prob_parm_device->rhoe_l = PeleC::prob_parm_device->rho_l * e_l;
 
-  EOS::RYP2E(ProbParm::rho_r, massfrac_r, ProbParm::p_r, e_r);
-  EOS::EY2T(e_r, massfrac_r, ProbParm::T_r);
-  ProbParm::rhoe_r = ProbParm::rho_r * e_r;
+  EOS::RYP2E(
+    PeleC::prob_parm_device->rho_r, massfrac_r, PeleC::prob_parm_device->p_r,
+    e_r);
+  EOS::EY2T(e_r, massfrac_r, PeleC::prob_parm_device->T_r);
+  PeleC::prob_parm_device->rhoe_r = PeleC::prob_parm_device->rho_r * e_r;
 }
 }
 
--- ExecCpp/RegTests/HIT/prob.cpp
+++ ExecCpp/RegTests/HIT/prob.cpp
@@ -1,64 +1,8 @@
 #include "prob.H"
 
-namespace ProbParm {
-std::string iname;
-AMREX_GPU_DEVICE_MANAGED bool binfmt = false;
-AMREX_GPU_DEVICE_MANAGED bool restart = false;
-AMREX_GPU_DEVICE_MANAGED amrex::Real lambda0 = 0.5;
-AMREX_GPU_DEVICE_MANAGED amrex::Real reynolds_lambda0 = 100.0;
-AMREX_GPU_DEVICE_MANAGED amrex::Real mach_t0 = 0.1;
-AMREX_GPU_DEVICE_MANAGED amrex::Real prandtl = 0.71;
-AMREX_GPU_DEVICE_MANAGED int inres = 0;
-AMREX_GPU_DEVICE_MANAGED amrex::Real uin_norm = 1.0;
-AMREX_GPU_DEVICE_MANAGED amrex::Real L_x = 0.0;
-AMREX_GPU_DEVICE_MANAGED amrex::Real L_y = 0.0;
-AMREX_GPU_DEVICE_MANAGED amrex::Real L_z = 0.0;
-AMREX_GPU_DEVICE_MANAGED amrex::Real Linput = 0.0;
-AMREX_GPU_DEVICE_MANAGED amrex::Real k0 = 0.0;
-AMREX_GPU_DEVICE_MANAGED amrex::Real rho0 = 0.0;
-AMREX_GPU_DEVICE_MANAGED amrex::Real urms0 = 0.0;
-AMREX_GPU_DEVICE_MANAGED amrex::Real tau = 0.0;
-AMREX_GPU_DEVICE_MANAGED amrex::Real p0 = 1.013e6; // [erg cm^-3]
-AMREX_GPU_DEVICE_MANAGED amrex::Real T0 = 300.0;
-AMREX_GPU_DEVICE_MANAGED amrex::Real eint0 = 0.0;
-amrex::Gpu::ManagedVector<amrex::Real>* v_xinput = nullptr;
-amrex::Gpu::ManagedVector<amrex::Real>* v_uinput = nullptr;
-amrex::Gpu::ManagedVector<amrex::Real>* v_vinput = nullptr;
-amrex::Gpu::ManagedVector<amrex::Real>* v_winput = nullptr;
-amrex::Gpu::ManagedVector<amrex::Real>* v_xarray = nullptr;
-amrex::Gpu::ManagedVector<amrex::Real>* v_xdiff = nullptr;
-
-AMREX_GPU_DEVICE_MANAGED amrex::Real* xinput = nullptr;
-AMREX_GPU_DEVICE_MANAGED amrex::Real* uinput = nullptr;
-AMREX_GPU_DEVICE_MANAGED amrex::Real* vinput = nullptr;
-AMREX_GPU_DEVICE_MANAGED amrex::Real* winput = nullptr;
-AMREX_GPU_DEVICE_MANAGED amrex::Real* xarray = nullptr;
-AMREX_GPU_DEVICE_MANAGED amrex::Real* xdiff = nullptr;
-
-} // namespace ProbParm
-
 void
 pc_prob_close()
 {
-  delete ProbParm::v_xinput;
-  delete ProbParm::v_uinput;
-  delete ProbParm::v_vinput;
-  delete ProbParm::v_winput;
-  delete ProbParm::v_xarray;
-  delete ProbParm::v_xdiff;
-
-  ProbParm::v_xinput = nullptr;
-  ProbParm::v_uinput = nullptr;
-  ProbParm::v_vinput = nullptr;
-  ProbParm::v_winput = nullptr;
-  ProbParm::v_xarray = nullptr;
-  ProbParm::v_xdiff = nullptr;
-  ProbParm::xinput = nullptr;
-  ProbParm::uinput = nullptr;
-  ProbParm::vinput = nullptr;
-  ProbParm::winput = nullptr;
-  ProbParm::xarray = nullptr;
-  ProbParm::xdiff = nullptr;
 }
 
 extern "C" {
@@ -73,15 +17,15 @@ amrex_probinit(
   // Parse params
   {
     amrex::ParmParse pp("prob");
-    pp.query("iname", ProbParm::iname);
-    pp.query("binfmt", ProbParm::binfmt);
-    pp.query("restart", ProbParm::restart);
-    pp.query("lambda0", ProbParm::lambda0);
-    pp.query("reynolds_lambda0", ProbParm::reynolds_lambda0);
-    pp.query("mach_t0", ProbParm::mach_t0);
-    pp.query("prandtl", ProbParm::prandtl);
-    pp.query("inres", ProbParm::inres);
-    pp.query("uin_norm", ProbParm::uin_norm);
+    pp.query("iname", PeleC::prob_parm_device->iname);
+    pp.query("binfmt", PeleC::prob_parm_device->binfmt);
+    pp.query("restart", PeleC::prob_parm_device->restart);
+    pp.query("lambda0", PeleC::prob_parm_device->lambda0);
+    pp.query("reynolds_lambda0", PeleC::prob_parm_device->reynolds_lambda0);
+    pp.query("mach_t0", PeleC::prob_parm_device->mach_t0);
+    pp.query("prandtl", PeleC::prob_parm_device->prandtl);
+    pp.query("inres", PeleC::prob_parm_device->inres);
+    pp.query("uin_norm", PeleC::prob_parm_device->uin_norm);
   }
 
   {
@@ -93,24 +37,28 @@ amrex_probinit(
   }
 
   // Define the length scale
-  ProbParm::L_x = probhi[0] - problo[0];
-  ProbParm::L_y = probhi[1] - problo[1];
-  ProbParm::L_z = probhi[2] - problo[2];
+  PeleC::prob_parm_device->L_x = probhi[0] - problo[0];
+  PeleC::prob_parm_device->L_y = probhi[1] - problo[1];
+  PeleC::prob_parm_device->L_z = probhi[2] - problo[2];
 
   // Wavelength associated to Taylor length scale
-  ProbParm::k0 = 2.0 / ProbParm::lambda0;
+  PeleC::prob_parm_device->k0 = 2.0 / PeleC::prob_parm_device->lambda0;
 
   // Initial density, velocity, and material properties
   amrex::Real cs;
   amrex::Real cp;
   amrex::Real massfrac[NUM_SPECIES] = {1.0};
   EOS::PYT2RE(
-    ProbParm::p0, massfrac, ProbParm::T0, ProbParm::rho0, ProbParm::eint0);
-  EOS::RTY2Cs(ProbParm::rho0, ProbParm::T0, massfrac, cs);
-  EOS::TY2Cp(ProbParm::T0, massfrac, cp);
+    PeleC::prob_parm_device->p0, massfrac, PeleC::prob_parm_device->T0,
+    PeleC::prob_parm_device->rho0, PeleC::prob_parm_device->eint0);
+  EOS::RTY2Cs(
+    PeleC::prob_parm_device->rho0, PeleC::prob_parm_device->T0, massfrac, cs);
+  EOS::TY2Cp(PeleC::prob_parm_device->T0, massfrac, cp);
 
-  ProbParm::urms0 = ProbParm::mach_t0 * cs / sqrt(3.0);
-  ProbParm::tau = ProbParm::lambda0 / ProbParm::urms0;
+  PeleC::prob_parm_device->urms0 =
+    PeleC::prob_parm_device->mach_t0 * cs / sqrt(3.0);
+  PeleC::prob_parm_device->tau =
+    PeleC::prob_parm_device->lambda0 / PeleC::prob_parm_device->urms0;
 
   TransParm trans_parm;
 
@@ -131,10 +79,12 @@ amrex_probinit(
 
   trans_parm.const_bulk_viscosity = 0.0;
   trans_parm.const_diffusivity = 0.0;
-  trans_parm.const_viscosity = ProbParm::rho0 * ProbParm::urms0 *
-                               ProbParm::lambda0 / ProbParm::reynolds_lambda0;
+  trans_parm.const_viscosity = PeleC::prob_parm_device->rho0 *
+                               PeleC::prob_parm_device->urms0 *
+                               PeleC::prob_parm_device->lambda0 /
+                               PeleC::prob_parm_device->reynolds_lambda0;
   trans_parm.const_conductivity =
-    trans_parm.const_viscosity * cp / ProbParm::prandtl;
+    trans_parm.const_viscosity * cp / PeleC::prob_parm_device->prandtl;
 
 #ifdef AMREX_USE_GPU
   amrex::Gpu::htod_memcpy(trans_parm_g, &trans_parm, sizeof(trans_parm));
@@ -149,12 +99,15 @@ amrex_probinit(
        "Mach, Prandtl, u0, v0, w0, forcing"
     << std::endl;
   amrex::Print(ofs).SetPrecision(17)
-    << ProbParm::lambda0 << "," << ProbParm::k0 << "," << ProbParm::rho0 << ","
-    << ProbParm::urms0 << "," << ProbParm::tau << "," << ProbParm::p0 << ","
-    << ProbParm::T0 << "," << EOS::gamma << "," << trans_parm.const_viscosity
-    << "," << trans_parm.const_conductivity << "," << cs << ","
-    << ProbParm::reynolds_lambda0 << "," << ProbParm::mach_t0 << ","
-    << ProbParm::prandtl << "," << forcing_params::u0 << ","
+    << PeleC::prob_parm_device->lambda0 << "," << PeleC::prob_parm_device->k0
+    << "," << PeleC::prob_parm_device->rho0 << ","
+    << PeleC::prob_parm_device->urms0 << "," << PeleC::prob_parm_device->tau
+    << "," << PeleC::prob_parm_device->p0 << "," << PeleC::prob_parm_device->T0
+    << "," << EOS::gamma << "," << trans_parm.const_viscosity << ","
+    << trans_parm.const_conductivity << "," << cs << ","
+    << PeleC::prob_parm_device->reynolds_lambda0 << ","
+    << PeleC::prob_parm_device->mach_t0 << ","
+    << PeleC::prob_parm_device->prandtl << "," << forcing_params::u0 << ","
     << forcing_params::v0 << "," << forcing_params::w0 << ","
     << forcing_params::forcing << std::endl;
   ofs.close();
@@ -166,73 +119,109 @@ amrex_probinit(
   // the input data is a periodic cube. If the input cube is smaller
   // than our domain size, the cube will be repeated throughout the
   // domain (hence the mod operations in the interpolation).
-  if (ProbParm::restart) {
+  if (PeleC::prob_parm_device->restart) {
     amrex::Print() << "Skipping input file reading and assuming restart."
                    << std::endl;
   } else {
 #ifdef AMREX_USE_FLOAT
     amrex::Abort("HIT cannot run in single precision at the moment.");
 #else
-    const size_t nx = ProbParm::inres;
-    const size_t ny = ProbParm::inres;
-    const size_t nz = ProbParm::inres;
+    const size_t nx = PeleC::prob_parm_device->inres;
+    const size_t ny = PeleC::prob_parm_device->inres;
+    const size_t nz = PeleC::prob_parm_device->inres;
     amrex::Vector<amrex::Real> data(
       nx * ny * nz * 6); /* this needs to be double */
-    if (ProbParm::binfmt) {
-      read_binary(ProbParm::iname, nx, ny, nz, 6, data);
+    if (PeleC::prob_parm_device->binfmt) {
+      read_binary(PeleC::prob_parm_device->iname, nx, ny, nz, 6, data);
     } else {
-      read_csv(ProbParm::iname, nx, ny, nz, data);
+      read_csv(PeleC::prob_parm_device->iname, nx, ny, nz, data);
     }
 
     // Extract position and velocities
-    ProbParm::v_uinput = new amrex::Gpu::ManagedVector<amrex::Real>;
-    ProbParm::v_vinput = new amrex::Gpu::ManagedVector<amrex::Real>;
-    ProbParm::v_winput = new amrex::Gpu::ManagedVector<amrex::Real>;
-    ProbParm::v_xarray = new amrex::Gpu::ManagedVector<amrex::Real>;
-    ProbParm::v_xinput = new amrex::Gpu::ManagedVector<amrex::Real>;
-    ProbParm::v_xdiff = new amrex::Gpu::ManagedVector<amrex::Real>;
-    ProbParm::v_xinput->resize(nx * ny * nz);
-    ProbParm::v_uinput->resize(nx * ny * nz);
-    ProbParm::v_vinput->resize(nx * ny * nz);
-    ProbParm::v_winput->resize(nx * ny * nz);
-    for (unsigned long i = 0; i < ProbParm::v_xinput->size(); i++) {
-      (*ProbParm::v_xinput)[i] = data[0 + i * 6];
-      (*ProbParm::v_uinput)[i] =
-        data[3 + i * 6] * ProbParm::urms0 / ProbParm::uin_norm;
-      (*ProbParm::v_vinput)[i] =
-        data[4 + i * 6] * ProbParm::urms0 / ProbParm::uin_norm;
-      (*ProbParm::v_winput)[i] =
-        data[5 + i * 6] * ProbParm::urms0 / ProbParm::uin_norm;
+    PeleC::prob_parm_host->h_xinput.resize(nx * ny * nz);
+    PeleC::prob_parm_host->h_uinput.resize(nx * ny * nz);
+    PeleC::prob_parm_host->h_vinput.resize(nx * ny * nz);
+    PeleC::prob_parm_host->h_winput.resize(nx * ny * nz);
+    for (long i = 0; i < PeleC::prob_parm_host->h_xinput.size(); i++) {
+      PeleC::prob_parm_host->h_xinput[i] = data[0 + i * 6];
+      PeleC::prob_parm_host->h_uinput[i] = data[3 + i * 6] *
+                                           PeleC::prob_parm_device->urms0 /
+                                           PeleC::prob_parm_device->uin_norm;
+      PeleC::prob_parm_host->h_vinput[i] = data[4 + i * 6] *
+                                           PeleC::prob_parm_device->urms0 /
+                                           PeleC::prob_parm_device->uin_norm;
+      PeleC::prob_parm_host->h_winput[i] = data[5 + i * 6] *
+                                           PeleC::prob_parm_device->urms0 /
+                                           PeleC::prob_parm_device->uin_norm;
     }
 
     // Get the xarray table and the differences.
-    ProbParm::v_xarray->resize(nx);
-    for (unsigned long i = 0; i < ProbParm::v_xarray->size(); i++) {
-      (*ProbParm::v_xarray)[i] = (*ProbParm::v_xinput)[i];
+    PeleC::prob_parm_host->h_xarray.resize(nx);
+    for (long i = 0; i < PeleC::prob_parm_host->h_xarray.size(); i++) {
+      PeleC::prob_parm_host->h_xarray[i] = PeleC::prob_parm_host->h_xinput[i];
     }
-    ProbParm::v_xdiff->resize(nx);
+    PeleC::prob_parm_host->h_xdiff.resize(nx);
     std::adjacent_difference(
-      ProbParm::v_xarray->begin(), ProbParm::v_xarray->end(),
-      ProbParm::v_xdiff->begin());
-    (*ProbParm::v_xdiff)[0] = (*ProbParm::v_xdiff)[1];
+      PeleC::prob_parm_host->h_xarray.begin(),
+      PeleC::prob_parm_host->h_xarray.end(),
+      PeleC::prob_parm_host->h_xdiff.begin());
+    PeleC::prob_parm_host->h_xdiff[0] = PeleC::prob_parm_host->h_xdiff[1];
 
     // Make sure the search array is increasing
     if (not std::is_sorted(
-          ProbParm::v_xarray->begin(), ProbParm::v_xarray->end())) {
+          PeleC::prob_parm_host->h_xarray.begin(),
+          PeleC::prob_parm_host->h_xarray.end())) {
       amrex::Abort("Error: non ascending x-coordinate array.");
     }
 
     // Get pointer to the data
-    ProbParm::xinput = ProbParm::v_xinput->dataPtr();
-    ProbParm::uinput = ProbParm::v_uinput->dataPtr();
-    ProbParm::vinput = ProbParm::v_vinput->dataPtr();
-    ProbParm::winput = ProbParm::v_winput->dataPtr();
-    ProbParm::xarray = ProbParm::v_xarray->dataPtr();
-    ProbParm::xdiff = ProbParm::v_xdiff->dataPtr();
+    PeleC::prob_parm_host->xinput.resize(
+      PeleC::prob_parm_host->h_xinput.size());
+    PeleC::prob_parm_host->uinput.resize(
+      PeleC::prob_parm_host->h_uinput.size());
+    PeleC::prob_parm_host->vinput.resize(
+      PeleC::prob_parm_host->h_vinput.size());
+    PeleC::prob_parm_host->winput.resize(
+      PeleC::prob_parm_host->h_winput.size());
+    PeleC::prob_parm_host->xarray.resize(
+      PeleC::prob_parm_host->h_xarray.size());
+    PeleC::prob_parm_host->xdiff.resize(PeleC::prob_parm_host->h_xdiff.size());
+    amrex::Gpu::copy(
+      amrex::Gpu::hostToDevice, PeleC::prob_parm_host->h_xinput.begin(),
+      PeleC::prob_parm_host->h_xinput.end(),
+      PeleC::prob_parm_host->xinput.begin());
+    amrex::Gpu::copy(
+      amrex::Gpu::hostToDevice, PeleC::prob_parm_host->h_uinput.begin(),
+      PeleC::prob_parm_host->h_uinput.end(),
+      PeleC::prob_parm_host->uinput.begin());
+    amrex::Gpu::copy(
+      amrex::Gpu::hostToDevice, PeleC::prob_parm_host->h_vinput.begin(),
+      PeleC::prob_parm_host->h_vinput.end(),
+      PeleC::prob_parm_host->vinput.begin());
+    amrex::Gpu::copy(
+      amrex::Gpu::hostToDevice, PeleC::prob_parm_host->h_winput.begin(),
+      PeleC::prob_parm_host->h_winput.end(),
+      PeleC::prob_parm_host->winput.begin());
+    amrex::Gpu::copy(
+      amrex::Gpu::hostToDevice, PeleC::prob_parm_host->h_xarray.begin(),
+      PeleC::prob_parm_host->h_xarray.end(),
+      PeleC::prob_parm_host->xarray.begin());
+    amrex::Gpu::copy(
+      amrex::Gpu::hostToDevice, PeleC::prob_parm_host->h_xdiff.begin(),
+      PeleC::prob_parm_host->h_xdiff.end(),
+      PeleC::prob_parm_host->xdiff.begin());
+
+    PeleC::prob_parm_device->d_xinput = PeleC::prob_parm_host->xinput.data();
+    PeleC::prob_parm_device->d_uinput = PeleC::prob_parm_host->uinput.data();
+    PeleC::prob_parm_device->d_vinput = PeleC::prob_parm_host->vinput.data();
+    PeleC::prob_parm_device->d_winput = PeleC::prob_parm_host->winput.data();
+    PeleC::prob_parm_device->d_xarray = PeleC::prob_parm_host->xarray.data();
+    PeleC::prob_parm_device->d_xdiff = PeleC::prob_parm_host->xdiff.data();
 
     // Dimensions of the input box.
-    ProbParm::Linput =
-      (*ProbParm::v_xarray)[nx - 1] + 0.5 * (*ProbParm::v_xdiff)[nx - 1];
+    PeleC::prob_parm_device->Linput =
+      PeleC::prob_parm_host->h_xarray[nx - 1] +
+      0.5 * PeleC::prob_parm_host->h_xdiff[nx - 1];
 #endif
   }
 }
