8a070563b4f3d000685e15adfccc4acdd2022604
Jon Rood <jon.rood@nrel.gov>
2021-02-10 17:16:03 -0700

    Increase test output limit for failed and passing tests.

--- Tests/CTestCustom.cmake
+++ Tests/CTestCustom.cmake
@@ -1,2 +1,4 @@
 SET(CTEST_CUSTOM_WARNING_EXCEPTION ".*: warning: could not create compact unwind*"
                                    ".*has no symbols*")
+set(CTEST_CUSTOM_MAXIMUM_FAILED_TEST_OUTPUT_SIZE "262144")
+set(CTEST_CUSTOM_MAXIMUM_PASSED_TEST_OUTPUT_SIZE "51200")
