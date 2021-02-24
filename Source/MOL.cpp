181308ed2c07cc8bdfa29822715587e57c49fd10
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2021-02-24 16:51:22 -0700

    Removing SRK ifdefs

--- Source/MOL.cpp
+++ Source/MOL.cpp
@@ -134,19 +134,12 @@ pc_compute_hyp_mol_flux(
         for (int n = 0; n < NUM_SPECIES; n++) {
           spl[n] = qtempl[R_Y + n];
         }
-#ifdef PELEPHYSICS_NONIDEAL_EOS
-        EOS::RYP2T(eos_state_rho, spl, eos_state_p, eos_state_T);
-        EOS::RTY2E(eos_state_rho, eos_state_T, spl, eos_state_e);
-        EOS::RTY2G(eos_state_rho, eos_state_T, spl, eos_state_gamma);
-        EOS::RTY2Cs(eos_state_rho, eos_state_T, spl, eos_state_cs);
-#else
         auto eos = pele::physics::PhysicsType::eos();
-        auto constants = pele::physics::Constants::RU;
         eos.RYP2T(eos_state_rho, spl, eos_state_p, eos_state_T);
-        eos.RYP2E(eos_state_rho, spl, eos_state_p, eos_state_e);
-        eos.TY2G(eos_state_T, spl, eos_state_gamma);
-        eos.RPY2Cs(eos_state_rho, eos_state_p, spl, eos_state_cs);
-#endif
+        eos.RTY2E(eos_state_rho, eos_state_T, spl, eos_state_e);
+        eos.RTY2G(eos_state_rho, eos_state_T, spl, eos_state_gamma);
+        eos.RTY2Cs(eos_state_rho, eos_state_T, spl, eos_state_cs);
+
         const amrex::Real rhoe_l = eos_state_rho * eos_state_e;
         const amrex::Real gamc_l = eos_state_gamma;
 
@@ -156,17 +149,11 @@ pc_compute_hyp_mol_flux(
         for (int n = 0; n < NUM_SPECIES; n++) {
           spr[n] = qtempr[R_Y + n];
         }
-#ifdef PELEPHYSICS_NONIDEAL_EOS
-        EOS::RYP2T(eos_state_rho, spr, eos_state_p, eos_state_T);
-        EOS::RTY2E(eos_state_rho, eos_state_T, spr, eos_state_e);
-        EOS::RTY2G(eos_state_rho, eos_state_T, spr, eos_state_gamma);
-        EOS::RTY2Cs(eos_state_rho, eos_state_T, spr, eos_state_cs);
-#else
         eos.RYP2T(eos_state_rho, spr, eos_state_p, eos_state_T);
-        eos.RYP2E(eos_state_rho, spr, eos_state_p, eos_state_e);
-        eos.TY2G(eos_state_T, spr, eos_state_gamma);
-        eos.RPY2Cs(eos_state_rho, eos_state_p, spr, eos_state_cs);
-#endif
+        eos.RTY2E(eos_state_rho, eos_state_T, spr, eos_state_e);
+        eos.RTY2G(eos_state_rho, eos_state_T, spr, eos_state_gamma);
+        eos.RTY2Cs(eos_state_rho, eos_state_T, spr, eos_state_cs);
+
         const amrex::Real rhoe_r = eos_state_rho * eos_state_e;
         const amrex::Real gamc_r = eos_state_gamma;
 
@@ -332,21 +319,12 @@ pc_compute_hyp_mol_flux(
       for (int n = 0; n < NUM_SPECIES; n++) {
         spl[n] = qtempl[R_Y + n];
       }
-#ifdef PELEPHYSICS_NONIDEAL_EOS
-      amrex::Real eos_state_T;
-      EOS::RYP2T(eos_state_rho, spl, eos_state_p, eos_state_T);
-      amrex::Real eos_state_e;
-      EOS::RTY2E(eos_state_rho, eos_state_T, spl, eos_state_e);
-      rhoe_l = eos_state_rho * eos_state_e;
-      EOS::RTY2G(eos_state_rho, eos_state_T, spl, gamc_l);
-#else
-      amrex::Real eos_state_e;
-      eos.RYP2E(eos_state_rho, spl, eos_state_p, eos_state_e);
-      rhoe_l = eos_state_rho * eos_state_e;
       amrex::Real eos_state_T;
       eos.RYP2T(eos_state_rho, spl, eos_state_p, eos_state_T);
-      eos.TY2G(eos_state_T, spl, gamc_l);
-#endif
+      amrex::Real eos_state_e;
+      eos.RTY2E(eos_state_rho, eos_state_T, spl, eos_state_e);
+      rhoe_l = eos_state_rho * eos_state_e;
+      eos.RTY2G(eos_state_rho, eos_state_T, spl, gamc_l);
     }
 
     if (is_inside(i, j, k, lo, hi, nextra - 1)) {
