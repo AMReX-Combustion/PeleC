d3e35dde085b72ea0ab2e96cefbab7baada5f79b
Landon Owen <ldowen@sandia.gov>
2021-03-31 11:26:06 -0700

    Updated soot for changes to development

--- Source/Soot.cpp
+++ Source/Soot.cpp
@@ -94,9 +94,11 @@ PeleC::fill_soot_source(
     {
       BL_PROFILE("PeleC::ctoprim()");
       PassMap const* lpmap = d_pass_map;
+      const int captured_clean_massfrac = clean_massfrac;
       amrex::ParallelFor(
         bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-          pc_ctoprim(i, j, k, s_arr, q_arr, qaux_arr, *lpmap);
+          pc_ctoprim(
+            i, j, k, s_arr, q_arr, qaux_arr, *lpmap, captured_clean_massfrac);
         });
     }
 
