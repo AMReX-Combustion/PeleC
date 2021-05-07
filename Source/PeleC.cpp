aeb14ff3495f219a19e6c25eb702f20267c994b6
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2021-05-07 08:32:32 -0600

    Only do redistribution if there is EB in the domain

--- Source/PeleC.cpp
+++ Source/PeleC.cpp
@@ -88,9 +88,9 @@ int PeleC::les_filter_fgr = 1;
 int PeleC::les_test_filter_type = box_3pt_optimized_approx;
 int PeleC::les_test_filter_fgr = 2;
 
+bool PeleC::eb_in_domain = false;
 #ifdef PELEC_USE_EB
 bool PeleC::eb_initialized = false;
-bool PeleC::eb_in_domain = false;
 bool PeleC::body_state_set = false;
 amrex::GpuArray<amrex::Real, NVAR> PeleC::body_state;
 #endif
@@ -2187,8 +2187,8 @@ PeleC::InitialRedistribution()
   // Next we must redistribute the initial solution if we are going to use
   // MergeRedist or StateRedist redistribution schemes
   if (
-    (redistribution_type != "StateRedist") &&
-    (redistribution_type != "MergeRedist")) {
+    (eb_in_domain) && ((redistribution_type != "StateRedist") &&
+                       (redistribution_type != "MergeRedist"))) {
     return;
   }
 
