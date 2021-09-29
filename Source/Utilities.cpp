34f0d6b72f1a00751cd431b7eceba6f226b60a1f
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2021-09-29 09:55:04 -0600

    Enabling better user defined EB geometry

--- Source/Utilities.cpp
+++ Source/Utilities.cpp
@@ -85,3 +85,11 @@ read_csv(
     }
   }
 }
+
+std::string
+convertIntGG(int number)
+{
+  std::stringstream ss; // create a stringstream
+  ss << number;         // add number to the stream
+  return ss.str();      // return a string with the contents of the stream
+}
