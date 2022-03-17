89e232d3372453589965555ff07880e8cbb38dff
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2022-03-17 08:58:13 -0600

    Fix clang-format. But one of these is strange

--- Source/PeleC.cpp
+++ Source/PeleC.cpp
@@ -766,8 +766,7 @@ PeleC::initialTimeStep()
   return init_dt;
 }
 
-amrex::Real
-PeleC::estTimeStep(amrex::Real /*dt_old*/)
+amrex::Real PeleC::estTimeStep(amrex::Real /*dt_old*/)
 {
   BL_PROFILE("PeleC::estTimeStep()");
 
@@ -1229,8 +1228,7 @@ PeleC::post_regrid(
   }
 }
 
-void
-PeleC::post_init(amrex::Real /*stop_time*/)
+void PeleC::post_init(amrex::Real /*stop_time*/)
 {
   BL_PROFILE("PeleC::post_init()");
 
