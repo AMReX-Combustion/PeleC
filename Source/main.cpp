2943addce8e7661acf7ca42d6bf153a01ccc73c2
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2022-06-13 11:19:13 -0600

    Mostly working?

--- Source/main.cpp
+++ Source/main.cpp
@@ -47,162 +47,221 @@ class myamr : public amrex::Amr
 public:
   void writePlotFile() override
   {
-    const bool hdf5 = true;
-    if (hdf5) {
-      if (!Plot_Files_Output()) {
-        return;
-      }
+    if (!Plot_Files_Output()) {
+      return;
+    }
+    BL_PROFILE_REGION_START("MyAmr::writePlotFile()");
+    BL_PROFILE("MyAmr::writePlotFile()");
 
-      BL_PROFILE_REGION_START("MyAmr::writePlotFile()");
-      BL_PROFILE("MyAmr::writePlotFile()");
+    if (first_plotfile) {
+      first_plotfile = false;
+      amr_level[0]->setPlotVariables();
+    }
 
-      auto dPlotFileTime0 = amrex::second();
+    // Don't continue if we have no variables to plot.
+    if (statePlotVars().size() == 0) {
+      return;
+    }
 
-      if (first_plotfile) {
-        first_plotfile = false;
-        amr_level[0]->setPlotVariables();
-      }
+    const std::string& pltfile =
+      amrex::Concatenate(plot_file_root, level_steps[0], file_name_digits);
 
-      // Don't continue if we have no variables to plot.
+    if (verbose > 0) {
+      amrex::Print() << "PLOTFILE: file = " << pltfile << '\n';
+    }
 
-      if (statePlotVars().size() == 0) {
-        return;
-      }
+    if (record_run_info && amrex::ParallelDescriptor::IOProcessor()) {
+      runlog << "PLOTFILE: file = " << pltfile << '\n';
+    }
 
-      const std::string& pltfile =
-        amrex::Concatenate(plot_file_root, level_steps[0], file_name_digits);
+    const bool hdf5 = false;
+    writePlotFileDoit(pltfile, true, hdf5);
 
-      if (verbose > 0) {
-        amrex::Print() << "PLOTFILE: file = " << pltfile << '\n';
-      }
+    BL_PROFILE_REGION_STOP("MyAmr::writePlotFile()");
+  }
 
-      if (record_run_info && amrex::ParallelDescriptor::IOProcessor()) {
-        runlog << "PLOTFILE: file = " << pltfile << '\n';
-      }
+  void writeSmallPlotFile() override
+  {
+    if (!Plot_Files_Output()) {
+      return;
+    }
 
-      // FIXME: make this common with the other
-      const auto& desc_lst = amrex::AmrLevel::get_desc_lst();
-      auto& derive_lst = amrex::AmrLevel::get_derive_lst();
+    BL_PROFILE_REGION_START("MyAmr::writeSmallPlotFile()");
+    BL_PROFILE("MyAmr::writeSmallPlotFile()");
 
-      amrex::Vector<std::pair<int, int>> plot_var_map;
-      for (int typ = 0; typ < desc_lst.size(); typ++) {
-        for (int comp = 0; comp < desc_lst[typ].nComp(); comp++) {
-          if (
-            amrex::Amr::isStatePlotVar(desc_lst[typ].name(comp)) &&
-            desc_lst[typ].getType() == amrex::IndexType::TheCellType()) {
-            plot_var_map.push_back(std::pair<int, int>(typ, comp));
-          }
+    if (first_smallplotfile) {
+      first_smallplotfile = false;
+      amr_level[0]->setSmallPlotVariables();
+    }
+
+    // Don't continue if we have no variables to plot.
+
+    if (stateSmallPlotVars().size() == 0) {
+      return;
+    }
+
+    const std::string& pltfile = amrex::Concatenate(
+      small_plot_file_root, level_steps[0], file_name_digits);
+
+    if (verbose > 0) {
+      amrex::Print() << "SMALL PLOTFILE: file = " << pltfile << '\n';
+    }
+
+    if (record_run_info && amrex::ParallelDescriptor::IOProcessor()) {
+      runlog << "SMALL PLOTFILE: file = " << pltfile << '\n';
+    }
+
+    const bool hdf5 = false;
+    writePlotFileDoit(pltfile, false, hdf5);
+
+    BL_PROFILE_REGION_STOP("MyAmr::writeSmallPlotFile()");
+  }
+
+  void writePlotFileDoit(
+    std::string const pltfile, const bool regular, const bool hdf5)
+  {
+
+    auto dPlotFileTime0 = amrex::second();
+
+    const auto& desc_lst = amrex::AmrLevel::get_desc_lst();
+    amrex::Vector<std::pair<int, int>> plot_var_map;
+    for (int typ = 0; typ < desc_lst.size(); typ++) {
+      for (int comp = 0; comp < desc_lst[typ].nComp(); comp++) {
+        if (
+          amrex::Amr::isStatePlotVar(desc_lst[typ].name(comp)) &&
+          desc_lst[typ].getType() == amrex::IndexType::TheCellType()) {
+          plot_var_map.push_back(std::pair<int, int>(typ, comp));
         }
       }
+    }
 
-      int num_derive = 0;
-      std::list<std::string> derive_names;
-      const std::list<amrex::DeriveRec>& dlist = derive_lst.dlist();
-
+    auto& derive_lst = amrex::AmrLevel::get_derive_lst();
+    std::list<std::string> derive_names;
+    const std::list<amrex::DeriveRec>& dlist = derive_lst.dlist();
+    if (regular) {
       for (const auto& it : dlist) {
         if (amrex::Amr::isDerivePlotVar(it.name())) {
           derive_names.push_back(it.name());
-          num_derive += it.numDerive();
         }
       }
+    }
 
-      const auto n_data_items = plot_var_map.size() + num_derive;
+    const auto n_data_items = plot_var_map.size() + derive_names.size();
 
-      const int nGrow = 0;
-      const amrex::Real cur_time =
-        (amr_level[0]->get_state_data(State_Type)).curTime();
+    const int nGrow = 0;
+    const amrex::Real cur_time =
+      (amr_level[0]->get_state_data(State_Type)).curTime();
 
-      const int nlevels = finestLevel() + 1;
-      amrex::Vector<std::unique_ptr<amrex::MultiFab>> plotMFs(nlevels);
-      amrex::Vector<int> istep(nlevels);
-      for (int lev = 0; lev < nlevels; ++lev) {
+    const int nlevels = finestLevel() + 1;
+    amrex::Vector<std::unique_ptr<amrex::MultiFab>> plotMFs(nlevels);
+    amrex::Vector<int> istep(nlevels);
+    for (int lev = 0; lev < nlevels; ++lev) {
 
-        istep[lev] = levelSteps(lev);
+      istep[lev] = levelSteps(lev);
 
-        plotMFs[lev] = std::make_unique<amrex::MultiFab>(
-          boxArray(lev), DistributionMap(lev), n_data_items, nGrow,
-          amrex::MFInfo(), amr_level[lev]->Factory());
+      plotMFs[lev] = std::make_unique<amrex::MultiFab>(
+        boxArray(lev), DistributionMap(lev), n_data_items, nGrow,
+        amrex::MFInfo(), amr_level[lev]->Factory());
 
-        // Cull data from state variables -- use no ghost cells.
-        int cnt = 0;
-        for (int i = 0; i < plot_var_map.size(); i++) {
-          int typ = plot_var_map[i].first;
-          int comp = plot_var_map[i].second;
-          amrex::MultiFab& this_dat = amr_level[lev]->get_new_data(typ);
-          amrex::MultiFab::Copy(*plotMFs[lev], this_dat, comp, cnt, 1, nGrow);
-          cnt++;
-        }
-
-        // Cull data from derived variables.
-        if (!derive_names.empty()) {
-          for (const auto& derive_name : derive_names) {
-            const amrex::DeriveRec* rec = derive_lst.get(derive_name);
-            int ncomp = rec->numDerive();
-
-            auto derive_dat =
-              amr_level[lev]->derive(derive_name, cur_time, nGrow);
-            amrex::MultiFab::Copy(
-              *plotMFs[lev], *derive_dat, 0, cnt, ncomp, nGrow);
-            cnt += ncomp;
-          }
-        }
-      }
-
-      amrex::Vector<std::string> plt_var_names;
+      // Cull data from state variables -- use no ghost cells.
+      int cnt = 0;
       for (int i = 0; i < plot_var_map.size(); i++) {
         int typ = plot_var_map[i].first;
         int comp = plot_var_map[i].second;
-        plt_var_names.push_back(desc_lst[typ].name(comp));
+        amrex::MultiFab& this_dat = amr_level[lev]->get_new_data(typ);
+        amrex::MultiFab::Copy(*plotMFs[lev], this_dat, comp, cnt, 1, nGrow);
+        cnt++;
       }
 
-      for (const auto& derive_name : derive_names) {
-        const amrex::DeriveRec* rec = derive_lst.get(derive_name);
-        for (int i = 0; i < rec->numDerive(); i++) {
-          plt_var_names.push_back(rec->variableName(i));
+      // Cull data from derived variables.
+      if ((!derive_names.empty())) {
+        for (const auto& derive_name : derive_names) {
+          const amrex::DeriveRec* rec = derive_lst.get(derive_name);
+          int ncomp = rec->numDerive();
+
+          auto derive_dat =
+            amr_level[lev]->derive(derive_name, cur_time, nGrow);
+          amrex::MultiFab::Copy(
+            *plotMFs[lev], *derive_dat, 0, cnt, ncomp, nGrow);
+          cnt += ncomp;
         }
       }
+    }
 
-      amrex::Vector<const amrex::MultiFab*> plotMFs_constvec;
-      plotMFs_constvec.reserve(nlevels);
-      for (int lev = 0; lev < nlevels; ++lev) {
-        plotMFs_constvec.push_back(
-          static_cast<const amrex::MultiFab*>(plotMFs[lev].get()));
+    std::string HeaderFileName(pltfile + "/Header");
+    amrex::VisMF::IO_Buffer io_buffer(amrex::VisMF::GetIOBufferSize());
+    std::ofstream HeaderFile;
+    HeaderFile.rdbuf()->pubsetbuf(io_buffer.dataPtr(), io_buffer.size());
+    if (amrex::ParallelDescriptor::IOProcessor()) {
+      // Only the IOProcessor() writes to the header file.
+      HeaderFile.open(
+        HeaderFileName.c_str(),
+        std::ios::out | std::ios::trunc | std::ios::binary);
+      if (!HeaderFile.good()) {
+        amrex::FileOpenFailed(HeaderFileName);
       }
+    }
 
+    amrex::Vector<std::string> plt_var_names;
+    for (int i = 0; i < plot_var_map.size(); i++) {
+      int typ = plot_var_map[i].first;
+      int comp = plot_var_map[i].second;
+      plt_var_names.push_back(desc_lst[typ].name(comp));
+    }
+
+    for (const auto& derive_name : derive_names) {
+      const amrex::DeriveRec* rec = derive_lst.get(derive_name);
+      for (int i = 0; i < rec->numDerive(); i++) {
+        plt_var_names.push_back(rec->variableName(i));
+      }
+    }
+
+    amrex::Vector<const amrex::MultiFab*> plotMFs_constvec;
+    plotMFs_constvec.reserve(nlevels);
+    for (int lev = 0; lev < nlevels; ++lev) {
+      plotMFs_constvec.push_back(
+        static_cast<const amrex::MultiFab*>(plotMFs[lev].get()));
+    }
+
+    if (regular) {
+      for (int lev = 0; lev < nlevels; ++lev) {
+        amr_level[lev]->writePlotFilePre(pltfile, HeaderFile);
+      }
+    }
+
+    if (hdf5) {
+      amrex::Abort("Not implemented yet");
+    } else {
       amrex::WriteMultiLevelPlotfile(
         pltfile, nlevels, plotMFs_constvec, plt_var_names, Geom(), cur_time,
         istep, refRatio());
+    }
 
-      std::string HeaderFileName(pltfile + "/Header");
-      amrex::VisMF::IO_Buffer io_buffer(amrex::VisMF::GetIOBufferSize());
-      std::ofstream HeaderFile;
-      HeaderFile.rdbuf()->pubsetbuf(io_buffer.dataPtr(), io_buffer.size());
-      if (amrex::ParallelDescriptor::IOProcessor()) {
-        // Only the IOProcessor() writes to the header file.
-        HeaderFile.open(
-          HeaderFileName.c_str(),
-          std::ios::out | std::ios::trunc | std::ios::binary);
-        if (!HeaderFile.good()) {
-          amrex::FileOpenFailed(HeaderFileName);
-        }
-      }
-
+    if (regular) {
       for (int lev = 0; lev < nlevels; ++lev) {
         amr_level[lev]->writePlotFilePost(pltfile, HeaderFile);
       }
+    }
 
-      if (verbose > 0) {
-        const int IOProc = amrex::ParallelDescriptor::IOProcessorNumber();
-        auto dPlotFileTime = amrex::second() - dPlotFileTime0;
-        amrex::ParallelDescriptor::ReduceRealMax(dPlotFileTime, IOProc);
+    if (regular) {
+      last_plotfile = level_steps[0];
+    } else {
+      last_smallplotfile = level_steps[0];
+    }
+
+    if (verbose > 0) {
+      const int IOProc = amrex::ParallelDescriptor::IOProcessorNumber();
+      auto dPlotFileTime = amrex::second() - dPlotFileTime0;
+      amrex::ParallelDescriptor::ReduceRealMax(dPlotFileTime, IOProc);
+      if (regular) {
         amrex::Print() << "Write plotfile time = " << dPlotFileTime
                        << "  seconds"
                        << "\n\n";
+      } else {
+        amrex::Print() << "Write small plotfile time = " << dPlotFileTime
+                       << "  seconds"
+                       << "\n\n";
       }
-
-      BL_PROFILE_REGION_STOP("MyAmr::writePlotFile()");
-    } else {
-      amrex::Amr::writePlotFile();
     }
   }
 };
