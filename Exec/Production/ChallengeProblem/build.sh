76faf38e5d77b70da4eb6c52d7ee72cbb50f3fa8
Jon Rood <jon.rood@nrel.gov>
2022-08-04 09:59:56 -0600

    Update challenge problem files (#537)

--- Exec/Production/ChallengeProblem/build.sh
+++ Exec/Production/ChallengeProblem/build.sh
@@ -11,15 +11,15 @@ if [ "${LMOD_SYSTEM_NAME}" == 'summit' ]; then
   cmd "module load cuda/11.4.2"
   cmd "module load cmake"
   cmd "module load binutils"
-  cmd "export SPACK_MANAGER=${PROJWORK}/cmb138/jrood/spack-manager-${LMOD_SYSTEM_NAME}"
-  ARGS="USE_CUDA=TRUE"
+  cmd "export SPACK_MANAGER=${WORLDWORK}/cmb138/software/spack-manager-${LMOD_SYSTEM_NAME}"
+  ARGS="USE_CUDA=TRUE PELE_USE_MAGMA=TRUE"
 elif [ "${LMOD_SYSTEM_NAME}" == 'crusher' ]; then
   cmd "module unload PrgEnv-cray"
   cmd "module load PrgEnv-amd"
   cmd "module load rocm/5.1.0"
   cmd "module load cmake"
-  cmd "export SPACK_MANAGER=${PROJWORK}/cmb138/jrood/spack-manager-${LMOD_SYSTEM_NAME}"
-  ARGS="USE_HIP=TRUE"
+  cmd "export SPACK_MANAGER=${WORLDWORK}/cmb138/software/spack-manager-${LMOD_SYSTEM_NAME}"
+  ARGS="USE_HIP=TRUE PELE_USE_MAGMA=TRUE"
 elif [ "${NREL_CLUSTER}" == 'eagle' ]; then
   cmd "source /nopt/nrel/ecom/hpacf/env.sh"
   cmd "module load gcc/9.3.0"
@@ -33,13 +33,13 @@ elif [ "${NREL_CLUSTER}" == 'eagle' ]; then
 fi
 
 cmd "source ${SPACK_MANAGER}/start.sh && spack-start"
-HDF5_HOME=$(spack location -i hdf5)
-H5Z_HOME=$(spack location -i h5z-zfp)
-ZFP_HOME=$(spack location -i zfp)
-ASCENT_DIR=$(spack location -i ascent)
-CONDUIT_DIR=$(spack location -i conduit)
+HDF5_ROOT=$(spack location -i hdf5)
+H5Z_ROOT=$(spack location -i h5z-zfp)
+ZFP_ROOT=$(spack location -i zfp)
+ASCENT_ROOT=$(spack location -i ascent)
+CONDUIT_ROOT=$(spack location -i conduit)
 
-ARGS="USE_MPI=TRUE TINY_PROFILE=TRUE USE_HDF5=TRUE USE_HDF5_ZFP=TRUE USE_ASCENT=TRUE USE_CONDUIT=TRUE HDF5_HOME=${HDF5_HOME} H5Z_HOME=${H5Z_HOME} ZFP_HOME=${ZFP_HOME} ASCENT_DIR=${ASCENT_DIR} CONDUIT_DIR=${CONDUIT_DIR} ${ARGS}"
+ARGS="USE_HDF5=TRUE USE_HDF5_ZFP=TRUE USE_ASCENT=TRUE USE_CONDUIT=TRUE HDF5_HOME=${HDF5_ROOT} H5Z_HOME=${H5Z_ROOT} ZFP_HOME=${ZFP_ROOT} ASCENT_DIR=${ASCENT_ROOT} CONDUIT_DIR=${CONDUIT_ROOT} ${ARGS}"
 
 cmd "make ${ARGS} TPLrealclean"
 cmd "make ${ARGS} realclean"
