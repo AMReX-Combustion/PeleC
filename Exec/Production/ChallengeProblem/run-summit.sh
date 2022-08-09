c7956b8c3c9ad832b5c36402a0cc3dbf332994ae
Jon Rood <jon.rood@nrel.gov>
2022-08-09 15:14:51 -0400

    Update Summit build and run scripts.

--- Exec/Production/ChallengeProblem/run-summit.sh
+++ Exec/Production/ChallengeProblem/run-summit.sh
@@ -13,8 +13,9 @@ cmd() {
 }
 
 cmd "module unload xl"
-cmd "module load gcc/9.3.0"
+cmd "module load gcc/10.2.0"
 cmd "module load cuda/11.4.2"
 cmd "module load cmake"
 cmd "module load binutils"
+cmd "module load netlib-lapack/3.9.1"
 cmd "jsrun -n 768 -r 6 -a 1 -c 1 -g 1 ./PeleC3d.gnu.TPROF.MPI.CUDA.ex challenge.inp max_step=500"
