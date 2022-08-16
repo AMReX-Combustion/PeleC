cb90a65ab81ffd8d6c05387767953c3f6b49e784
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2022-08-16 13:51:58 -0600

    Forgot files

---
+++ Source/PeleAscent.cpp
@@ -0,0 +1,14 @@
+#ifdef AMREX_USE_ASCENT
+#include <AMReX_ParmParse.H>
+#include "PeleAscent.H"
+
+
+namespace pele {
+PeleAscent::PeleAscent(){
+  {
+    amrex::ParmParse pp("ascent");
+    pp.query("plot_int", plot_int);
+  }
+};
+} // namespace pele
+#endif
