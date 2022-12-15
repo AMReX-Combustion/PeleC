fbecc3a17601cbc4e4678fa7bc978f326d39ea2e
Marc T. Henry de Frahan <marc.henrydefrahan@nrel.gov>
2022-12-15 10:58:36 -0700

    Update submodules and remove elixirs (#575)
    * Remove elixirs and update submodules.
    
    * Update PelePhysics.
    
    Co-authored-by: Jon Rood <jon.rood@nrel.gov>

--- Source/Derive.cpp
+++ Source/Derive.cpp
@@ -261,8 +261,7 @@ pc_dermagvort(
 
   const amrex::Box& gbx = amrex::grow(bx, 1);
 
-  amrex::FArrayBox local(gbx, 3);
-  amrex::Elixir local_eli = local.elixir();
+  amrex::FArrayBox local(gbx, 3, amrex::The_Async_Arena());
   auto larr = local.array();
 
   const auto& flag_fab = amrex::getEBCellFlagFab(datfab);
@@ -383,8 +382,7 @@ pc_derenstrophy(
 
   const amrex::Box& gbx = amrex::grow(bx, 1);
 
-  amrex::FArrayBox local(gbx, 3);
-  amrex::Elixir local_eli = local.elixir();
+  amrex::FArrayBox local(gbx, 3, amrex::The_Async_Arena());
   auto larr = local.array();
 
   const auto& flag_fab = amrex::getEBCellFlagFab(datfab);
