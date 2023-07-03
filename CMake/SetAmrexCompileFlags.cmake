c58198a96710c57c3ac85da0d5c319cb323fe7d7
Bruce Perry <bruce.perry@nrel.gov>
2023-07-03 15:46:37 -0600

    fix amrex dim alias cmake issue

--- CMake/SetAmrexCompileFlags.cmake
+++ CMake/SetAmrexCompileFlags.cmake
@@ -1,10 +1,19 @@
 # Disable loop not vectorized warnings on Clang. This generates a lot of
 # diagnostic messages when compiling AMReX that we can't do anything about
 if(CMAKE_CXX_COMPILER_ID MATCHES "^(Clang|AppleClang)$")
-  target_compile_options(
-    amrex PUBLIC $<$<COMPILE_LANGUAGE:CXX>:-Wno-pass-failed>)
-  if((CMAKE_CXX_COMPILER_ID STREQUAL "Clang") AND PELEC_ENABLE_FPE_TRAP_FOR_TESTS)
+  if(PELEC_DIM EQUAL 3)
     target_compile_options(
-      amrex PUBLIC $<$<COMPILE_LANGUAGE:CXX>:-ffp-exception-behavior=maytrap>)
+      amrex_3d PUBLIC $<$<COMPILE_LANGUAGE:CXX>:-Wno-pass-failed>)
+    if((CMAKE_CXX_COMPILER_ID STREQUAL "Clang") AND PELEC_ENABLE_FPE_TRAP_FOR_TESTS)
+      target_compile_options(
+        amrex_3d PUBLIC $<$<COMPILE_LANGUAGE:CXX>:-ffp-exception-behavior=maytrap>)
+    endif()
+  else()
+    target_compile_options(
+      amrex_2d PUBLIC $<$<COMPILE_LANGUAGE:CXX>:-Wno-pass-failed>)
+    if((CMAKE_CXX_COMPILER_ID STREQUAL "Clang") AND PELEC_ENABLE_FPE_TRAP_FOR_TESTS)
+      target_compile_options(
+        amrex_2d PUBLIC $<$<COMPILE_LANGUAGE:CXX>:-ffp-exception-behavior=maytrap>)
+    endif()
   endif()
 endif()
