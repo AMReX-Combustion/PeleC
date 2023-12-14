20d720da7cba372420d48b5901de60cfdac50fc9
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2023-12-14 08:51:55 -0700

    some more fixes

--- Exec/RegTests/Spray-EB/SprayParticlesInitInsert.cpp
+++ Exec/RegTests/Spray-EB/SprayParticlesInitInsert.cpp
@@ -17,9 +17,14 @@ SprayParticleContainer::injectParticles(
   if (lev != 0) {
     return false;
   }
-  SprayJet* js = m_sprayJets[0].get();
 
-  sprayInjection(time, js, dt, lev);
+  bool do_inject = false;
+  amrex::ParmParse ps("spray.jet1");
+  ps.query("do_inject", do_inject);
+  if (do_inject) {
+    SprayJet* js = m_sprayJets[0].get();
+    sprayInjection(time, js, dt, lev);
+  }
 
   // Redistribute is done outside of this function
   return true;
@@ -32,13 +37,12 @@ SprayParticleContainer::InitSprayParticles(
   ProbParmDevice const& prob_parm_d)
 {
   amrex::ignore_unused(prob_parm, init_parts);
-  m_sprayJets.resize(1);
   std::string jet_name = "jet1";
   bool do_inject = false;
   amrex::ParmParse ps("spray.jet1");
   ps.query("do_inject", do_inject);
   if (do_inject) {
-    m_sprayJets[0] = std::make_unique<SprayJet>(jet_name, Geom(0));
+    m_sprayJets.push_back(std::make_unique<SprayJet>(jet_name, Geom(0)));
   }
   return;
 }
