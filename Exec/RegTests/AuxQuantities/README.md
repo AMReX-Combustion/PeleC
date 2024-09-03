3053d4203723c6d67963c6406f3f054083095095
dmontgomeryNREL <David.Montgomery@nrel.gov>
2024-09-03 15:55:43 -0600

    Updated the strength of the source term so the B_k's decay faster.  Still seeing some "transport" around the EB.

--- Exec/RegTests/AuxQuantities/README.md
+++ Exec/RegTests/AuxQuantities/README.md
@@ -1,6 +1,6 @@
 # Advected and auxiliary quantities
 
-This demonstrates the use of the advected and auxiliary quantities described in the PeleC model [equations](https://amrex-combustion.github.io/PeleC/Equations.html). The case introduces two advected and two auxiliary quantities into the domain.  The auxiliary quantities experience exponential decay with a source term of $S_{ext,\rho B_k} = -30(\rho B_k)$.
+This demonstrates the use of the advected and auxiliary quantities described in the PeleC model [equations](https://amrex-combustion.github.io/PeleC/Equations.html). The case introduces two advected and two auxiliary quantities into the domain.  The auxiliary quantities experience exponential decay with a source term of $S_{ext,\rho B_k} = -8500 \rho B_k$.
 
 
 ## Short case description
--- Exec/RegTests/AuxQuantities/prob.cpp
+++ Exec/RegTests/AuxQuantities/prob.cpp
@@ -65,23 +65,19 @@ amrex_probinit(
   // Output IC
   amrex::Print() << "\nGenerate ic.txt" << std::endl;
   std::ofstream ofs("ic.txt", std::ofstream::out);
-  amrex::Print(ofs) << "L = " << L << "\nH = " << H
-                    << "\np = " << PeleC::h_prob_parm_device->p
-                    << "\nT = " << PeleC::h_prob_parm_device->T
-                    << "\ngamma = " << eos.gamma << "\ncs = " << cs
-                    << "\nU = " << PeleC::h_prob_parm_device->U
-                    << "\nrho = " << PeleC::h_prob_parm_device->rho
-                    << "\nviscosity = " << trans_parm.const_viscosity
-                    << "\nconductivity = " << trans_parm.const_conductivity
-                    << "\nRe = " << PeleC::h_prob_parm_device->Re
-                    << "\nMa = " << PeleC::h_prob_parm_device->Ma
-                    << "\nPr = " << PeleC::h_prob_parm_device->Pr
-                    << "\nNUM_AUX = " << NUM_AUX
-                    << "\naux_xy_lo =" << PeleC::h_prob_parm_device->aux_xy_lo
-                    << "\naux_xy_length ="
-                    << PeleC::h_prob_parm_device->aux_length
-                    << "\naux_xy_height"
-                    << PeleC::h_prob_parm_device->aux_height << std::endl;
+  amrex::Print(ofs)
+    << "L = " << L << "\nH = " << H << "\np = " << PeleC::h_prob_parm_device->p
+    << "\nT = " << PeleC::h_prob_parm_device->T << "\ngamma = " << eos.gamma
+    << "\ncs = " << cs << "\nU = " << PeleC::h_prob_parm_device->U
+    << "\nrho = " << PeleC::h_prob_parm_device->rho
+    << "\nviscosity = " << trans_parm.const_viscosity
+    << "\nconductivity = " << trans_parm.const_conductivity
+    << "\nRe = " << PeleC::h_prob_parm_device->Re
+    << "\nMa = " << PeleC::h_prob_parm_device->Ma
+    << "\nPr = " << PeleC::h_prob_parm_device->Pr << "\nNUM_AUX = " << NUM_AUX
+    << "\naux_xy_lo = " << PeleC::h_prob_parm_device->aux_xy_lo
+    << "\naux_length = " << PeleC::h_prob_parm_device->aux_length
+    << "\naux_height = " << PeleC::h_prob_parm_device->aux_height << std::endl;
   ofs.close();
 }
 }
