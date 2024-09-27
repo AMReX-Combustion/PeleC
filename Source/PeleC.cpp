17d4525895de1f77701dc5ae6db22c7bf40cec57
Hariswaran Sitaraman <hsitaram@kl2.head.cm.kestrel.hpc.nrel.gov>
2024-09-27 11:08:53 -0600

    removing verbose

--- Source/PeleC.cpp
+++ Source/PeleC.cpp
@@ -2200,7 +2200,6 @@ PeleC::reset_internal_energy(amrex::MultiFab& S_new, int ng)
     const auto captured_allow_negative_energy = allow_negative_energy;
     const auto captured_dual_energy_update_E_from_e =
       dual_energy_update_E_from_e;
-    const auto captured_verbose = verbose;
     const auto captured_dual_energy_eta2 = dual_energy_eta2;
     auto sarrs = S_new.arrays();
     const amrex::IntVect ngs(ng);
@@ -2217,7 +2216,7 @@ PeleC::reset_internal_energy(amrex::MultiFab& S_new, int ng)
         pc_rst_int_e(
           i, j, k, sarrs[nbx], captured_allow_small_energy,
           captured_allow_negative_energy, captured_dual_energy_update_E_from_e,
-          captured_dual_energy_eta2, omega, rad, captured_verbose);
+          captured_dual_energy_eta2, omega, rad);
       });
     amrex::Gpu::synchronize();
   }
