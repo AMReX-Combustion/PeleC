a17638d8bf1f5e1fda886dad93c4d813209db6bf
hsitaram <hariswaran@gmail.com>
2024-09-27 22:04:15 -0600

    fixing 2d bug

--- Source/PeleC.cpp
+++ Source/PeleC.cpp
@@ -2205,21 +2205,35 @@ PeleC::reset_internal_energy(amrex::MultiFab& S_new, int ng)
     const amrex::IntVect ngs(ng);
 
     // for Rotational frames
-    const auto geomdata = geom.data();
-    int axis = rf_axis;
-    amrex::Real omega = rf_omega;
-    amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> axis_loc = {
-      AMREX_D_DECL(rf_axis_x, rf_axis_y, rf_axis_z)};
-    amrex::ParallelFor(
-      S_new, ngs, [=] AMREX_GPU_DEVICE(int nbx, int i, int j, int k) noexcept {
-        amrex::IntVect iv(AMREX_D_DECL(i, j, k));
-        amrex::Real rad = get_rotaxis_dist(iv, axis, axis_loc, geomdata);
-        pc_rst_int_e(
-          i, j, k, sarrs[nbx], captured_allow_small_energy,
-          captured_allow_negative_energy, captured_dual_energy_update_E_from_e,
-          captured_dual_energy_eta2, omega, rad);
-      });
-    amrex::Gpu::synchronize();
+    if (do_rf) {
+      const auto geomdata = geom.data();
+      int axis = rf_axis;
+      amrex::Real omega = rf_omega;
+      amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> axis_loc = {
+        AMREX_D_DECL(rf_axis_x, rf_axis_y, rf_axis_z)};
+      amrex::ParallelFor(
+        S_new, ngs,
+        [=] AMREX_GPU_DEVICE(int nbx, int i, int j, int k) noexcept {
+          amrex::IntVect iv(AMREX_D_DECL(i, j, k));
+          amrex::Real rad = get_rotaxis_dist(iv, axis, axis_loc, geomdata);
+          pc_rst_int_e(
+            i, j, k, sarrs[nbx], captured_allow_small_energy,
+            captured_allow_negative_energy,
+            captured_dual_energy_update_E_from_e, captured_dual_energy_eta2,
+            omega, rad);
+        });
+      amrex::Gpu::synchronize();
+    } else {
+      amrex::ParallelFor(
+        S_new, ngs,
+        [=] AMREX_GPU_DEVICE(int nbx, int i, int j, int k) noexcept {
+          pc_rst_int_e(
+            i, j, k, sarrs[nbx], captured_allow_small_energy,
+            captured_allow_negative_energy,
+            captured_dual_energy_update_E_from_e, captured_dual_energy_eta2);
+        });
+      amrex::Gpu::synchronize();
+    }
   }
 
 #ifndef AMREX_USE_GPU
